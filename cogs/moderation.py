d1a412cfad663d6959b997d3609f92dde8e13972
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-26 02:46:15 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,10 +1,24 @@
+from datetime import datetime
 import discord
 from discord.ext import commands
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.start_time = datetime.now()
 
+    @commands.command()
+    async def uptime(self, ctx):
+        current_time = datetime.now()
+        uptime = current_time - self.start_time
+
+        days = uptime.days
+        hours, remainder = divmod(uptime.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        uptime_str = f"{days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
+
+        await ctx.reply(f"Bot Uptime: {uptime_str}")
 
 
 
@@ -32,7 +46,11 @@ class Moderation(commands.Cog):
         await member.ban(reason=reason)
         await ctx.send(f'Banned {member.mention} for reason: {reason}')
 
+    @commands.command()
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)  # Convert to milliseconds
 
+        await ctx.reply(f"Pong! :ping_pong: Latency: {latency}ms")
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
