1f51bd8fcebdc15ac5b9ec90230ad6f3eb8cb790
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-27 16:45:33 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -7,8 +7,7 @@ class welcandleave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        guild_id = 1121841073673736215
-        if member.guild.id == guild_id:
+        if member.guild.id == 1121841073673736215:
             embed = discord.Embed(title='Welcome to Kanzen!', color=0x2b2d31, description=f"• Read the [rules](https://discord.com/channels/1121841073673736215/1121913361169391666)\n• Go and get your [roles](https://discord.com/channels/1121841073673736215/1121922077071507476)\n• need help? ping <@&1121842279351590973>!")
             embed.set_footer(text='Hope you enjoy your stay!', icon_url=member.display_avatar.url)
             channel = self.bot.get_channel(1121921106706710567)
@@ -18,11 +17,10 @@ class welcandleave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        guild_id = 1121841073673736215
-        if member.guild.id == guild_id:
-            embed = discord.Embed(title="Member left!", color=0x96bfff, description=f"{member.mention} has left the discord.")
+        if member.guild.id == 1121841073673736215:
+            embed = discord.Embed(title=f"{member.name} has left kanzen!", color=0x2b2d31, description=f"{member.mention} We will miss you!")
             embed.set_thumbnail(url=member.display_avatar.url)
-            embed.set_footer(text='i hope to see you again!')
+            embed.set_footer(text='Hope to see you again <3')
             channel = self.bot.get_channel(1121921106706710567)
             await channel.send(embed=embed)
 
