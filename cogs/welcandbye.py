9a77e350b101602581a1efded15ee178c1244a5a
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-29 00:59:22 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -78,14 +78,41 @@ class welcandleave(commands.Cog):
                 channel_id = 1123649005147127818 #kanzen channel
             elif server_id == 1123347338841313331: #aura forms
                 channel_id = 1123752056772104252 #aura forms channel
-            elif server_id == 1122181605591621692:
-                channel_id = 1123755909089337364
+            elif server_id == 1122181605591621692: #kanzengrp forms
+                channel_id = 1123755909089337364 #kanzen forms channel
             
             if channel_id:
                 channel = self.bot.get_channel(channel_id)
                 embed = discord.Embed(title=f"{after.name} thank you for boosting!",description='<a:greenarrow:1123286634629169203> do `+perks` to get the booster perks!\n<a:greenarrow:1123286634629169203> dm a lead or co lead for your custom role\n<a:greenarrow:1123286634629169203> we really appriciate the support', color=0x2b2d31)
                 await channel.send(f'{after.mention}')
                 await channel.send(embed=embed)
+
+    @commands.command()
+    async def perks(self, ctx):
+        server_id = ctx.guild.id
+        
+        if server_id in [957987670787764224, 1123347338841313331]:
+            embed = discord.Embed(title="Perks", description="These are the perks for Server 1 and Server 2", color=discord.Color.blue())
+            embed.add_field(name="Perk 1", value="Perk 1 description", inline=False)
+            embed.add_field(name="Perk 2", value="Perk 2 description", inline=False)
+            embed.set_footer(text="Powered by MyBot")
+
+            if ctx.author.premium_since:
+                await ctx.author.send(embed=embed)
+            else:
+                await ctx.send("Sorry, this command is only available for server boosters.")
+
+        elif server_id in [1121841073673736215, 1122181605591621692]:
+            embed = discord.Embed(title="Perks", description="These are the perks for Server 3 and Server 4", color=discord.Color.green())
+            embed.add_field(name="Perk 3", value="Perk 3 description", inline=False)
+            embed.add_field(name="Perk 4", value="Perk 4 description", inline=False)
+            embed.set_footer(text="Powered by MyBot")
+
+            if ctx.author.premium_since:
+                await ctx.author.send(embed=embed)
+            else:
+                await ctx.send("Sorry, this command is only available for server boosters.")
+
 
 async def setup(bot):
     await bot.add_cog(welcandleave(bot))
