07c3b4d5346eefc340712542cefc32a3f258fa1c
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-28 20:35:49 +0100

    Update qna.py

--- cogs/qna.py
+++ cogs/qna.py
@@ -5,52 +5,47 @@ from discord.ext import commands
 class qna(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.question_channel_id = 1123696501441450107
+        self.question_channel_id = 1123696243911164054
         self.answer_channel_id = 1123696762985656451
+        self.deleted_messages_channel_id = 1123696501441450107
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == 1123696243911164054:
+        if message.channel.id == self.question_channel_id:
             await self.process_question(message)
+        elif message.channel.id == self.answer_channel_id:
+            await self.process_answer(message)
 
     async def process_question(self, message):
         question_channel = self.bot.get_channel(self.question_channel_id)
-        answer_channel = self.bot.get_channel(self.answer_channel_id)
+        deleted_messages_channel = self.bot.get_channel(self.deleted_messages_channel_id)
+
+        # Create an embed with the deleted message and user information
+        embed = discord.Embed(title="Deleted Question", color=discord.Color.red())
+        embed.add_field(name="Content", value=message.content)
+        embed.add_field(name="User ID", value=message.author.id)
+
+        # Send the embed to the deleted messages channel
+        await deleted_messages_channel.send(embed=embed)
 
         # Delete the question message
         await message.delete()
 
-        # Create an embed with the question and user information
-        embed = discord.Embed(title="Question", description=message.content, color=discord.Color.blue())
-        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+    async def process_answer(self, message):
+        answer_channel = self.bot.get_channel(self.answer_channel_id)
+        deleted_messages_channel = self.bot.get_channel(self.deleted_messages_channel_id)
 
-        # Send the embed to the question channel
-        question_msg = await question_channel.send(embed=embed)
+        # Create an embed with the deleted message and user information
+        embed = discord.Embed(title="Deleted Answer", color=discord.Color.red())
+        embed.add_field(name="Content", value=message.content)
+        embed.add_field(name="User ID", value=message.author.id)
 
-        # Add a reaction to the question message for easier answering
-        await question_msg.add_reaction("✉️")
+        # Send the embed to the deleted messages channel
+        await deleted_messages_channel.send(embed=embed)
 
-        # Wait for an answer
-        def check_answer(reaction, user):
-            return (
-                reaction.message.id == question_msg.id
-                and str(reaction.emoji) == "✉️"
-                and user != self.bot.user
-            )
+        # Delete the answer message
+        await message.delete()
 
-        try:
-            reaction, user = await self.bot.wait_for("reaction_add", timeout=300.0, check=check_answer)
-            answer_content = reaction.message.content
-
-            # Create an embed with the answer and user information
-            answer_embed = discord.Embed(title="Answer", description=answer_content, color=discord.Color.green())
-            answer_embed.set_author(name=user.name, icon_url=user.avatar_url)
-
-            # Ping the user who asked the question and send the answer embed to the answer channel
-            answer_embed.set_footer(text=f"In response to {message.author.name}")
-            await answer_channel.send(f"<@{message.author.id}>", embed=answer_embed)
-        except asyncio.TimeoutError:
-            await question_channel.send("No answer received within 5 minutes.")
 
 async def setup(bot):
     await bot.add_cog(qna(bot))
