721327c4d20ef00db0bec897ccffc1f0a7335aca
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-28 22:00:12 +0100

    Update qna.py

--- cogs/qna.py
+++ cogs/qna.py
@@ -5,18 +5,25 @@ from discord.ext import commands
 class qna(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.deleted_messages_channel_id = 1123696501441450107
-        self.answer_channel_id = 1123696762985656451
+        self.server1_deleted_messages_channel_id = 1123696501441450107
+        self.server1_answer_channel_id = 1123696762985656451
+        self.server2_deleted_messages_channel_id = 1234567890
+        self.server2_answer_channel_id = 2345678901
 
     @commands.command(hidden=True)
     async def answer(self, ctx, *, response: str):
         reference = ctx.message.reference
         if reference and reference.message_id:
             msg = await ctx.channel.fetch_message(reference.message_id)
-            if "|" in msg.content:
-                question, asker = msg.content.split("| ")
+            if "^" in msg.content:
+                question, asker = msg.content.split("^ ")
                 user = await self.bot.fetch_user(int(asker))
-                answer_channel = self.bot.get_channel(self.answer_channel_id)
+                if ctx.guild.id == 1123347338841313331:  # Server 1
+                    answer_channel = self.bot.get_channel(self.server1_answer_channel_id)
+                elif ctx.guild.id == 1122181605591621692:  # Server 2
+                    answer_channel = self.bot.get_channel(self.server2_answer_channel_id)
+                else:
+                    return
 
                 embed = discord.Embed(title="kanzen q&n", color=0x2b2d31, description=f"**question:** {question}**\nanswer:** {response}")
                 embed.set_footer(text=f"asked by {user.display_name} | answered by {ctx.author.display_name}")
@@ -29,11 +36,16 @@ class qna(commands.Cog):
 
     @commands.command()
     async def qna(self, ctx, *, question: str):
-        deleted_messages_channel = self.bot.get_channel(self.deleted_messages_channel_id)
+        if ctx.guild.id == 1123347338841313331:  # Server 1
+            deleted_messages_channel = self.bot.get_channel(self.server1_deleted_messages_channel_id)
+        elif ctx.guild.id == 1122181605591621692:  # Server 2
+            deleted_messages_channel = self.bot.get_channel(self.server2_deleted_messages_channel_id)
+        else:
+            return
 
         q = await ctx.reply("Your question has been sent to the lead!")
 
-        message = f"{question} | {ctx.author.id}"
+        message = f"{question} ^ {ctx.author.id}"
         await deleted_messages_channel.send(message)
 
         await asyncio.sleep(3)
