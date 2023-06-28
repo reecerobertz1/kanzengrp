86d09ba61f365a54e3e2274c36e872a2da5a1226
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-28 17:37:14 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -24,5 +24,59 @@ class logos(commands.Cog):
 
         await ctx.send(embed=embed, view=view)
 
+    @commands.command()
+    async def daegupartner(self, ctx):
+        embed = discord.Embed(
+            title='<:members:1119069738493026364> : DAEGUTOWN FORMS',
+            description='<a:redarrow:1123372633182982155> editing server\n<a:redarrow:1123372633182982155> amazing logos + custom bot\n<a:redarrow:1123372633182982155> booster pack | booster form',
+            color=0x9e4343
+        )
+
+        button = discord.ui.Button(
+            label='Click here to join Daegu Forms!',
+            url='https://discord.gg/R5d7TXVdcj'
+        )
+
+        view = discord.ui.View()
+        view.add_item(button)
+
+        await ctx.send(embed=embed, view=view)
+
+    @commands.command()
+    async def aurapartner(self, ctx):
+        embed = discord.Embed(
+            title='<:frogyay:1123652434913402950> : Aura Forms',
+            description='<a:greenarrow:1123286634629169203> editing server\n<a:greenarrow:1123286634629169203> cute aesthetic logos\n<a:greenarrow:1123286634629169203> custom bot',
+            color=0x2b2d31
+        )
+
+        button = discord.ui.Button(
+            label='Click here to join Aura Forms!',
+            url='https://discord.gg/edDCM3JUvM'
+        )
+
+        view = discord.ui.View()
+        view.add_item(button)
+
+        await ctx.send(embed=embed, view=view)
+
+    @commands.command()
+    async def kanzenpartner(self, ctx):
+        embed = discord.Embed(
+            title='<a:Lumi_penguin_fall:1122607666578063531> : Kanzen Forms',
+            description='• editing group\n• cute logos\n• custom bot',
+            color=0x2b2d31
+        )
+
+        button = discord.ui.Button(
+            label='Click here to join Kanzen Forms!',
+            url='https://discord.gg/9MqbUTYthE'
+        )
+
+        view = discord.ui.View()
+        view.add_item(button)
+
+        await ctx.send(embed=embed, view=view)
+
 async def setup(bot):
     await bot.add_cog(logos(bot))
