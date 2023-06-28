7e1a1ed9bf426e85bc4faf402c844f2e8058ad32
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-28 20:51:13 +0100

    Update qna.py

--- cogs/qna.py
+++ cogs/qna.py
@@ -5,52 +5,69 @@ from discord.ext import commands
 class qna(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.question_channel_id = 1123696501441450107
+        self.question_channel_id = 1123696243911164054
         self.answer_channel_id = 1123696762985656451
+        self.deleted_messages_channel_id = 1123696501441450107
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == 1123696243911164054:
+        if message.channel.id == self.question_channel_id:
             await self.process_question(message)
+        elif message.channel.id == self.answer_channel_id:
+            await self.process_answer(message)
 
     async def process_question(self, message):
         question_channel = self.bot.get_channel(self.question_channel_id)
-        answer_channel = self.bot.get_channel(self.answer_channel_id)
+        deleted_messages_channel = self.bot.get_channel(self.deleted_messages_channel_id)
+
+        # Send the deleted question content and user ID to the deleted messages channel
+        deleted_message = f"Deleted Question\nContent: {message.content}\nUser ID: {message.author.id}"
+        await deleted_messages_channel.send(deleted_message)
 
         # Delete the question message
         await message.delete()
 
-        # Create an embed with the question and user information
-        embed = discord.Embed(title="Question", description=message.content, color=discord.Color.blue())
-        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+    async def process_answer(self, message):
+        answer_channel = self.bot.get_channel(self.answer_channel_id)
+        deleted_messages_channel = self.bot.get_channel(self.deleted_messages_channel_id)
 
-        # Send the embed to the question channel
-        question_msg = await question_channel.send(embed=embed)
+        # Send the deleted answer content and user ID to the deleted messages channel
+        deleted_message = f"Deleted Answer\nContent: {message.content}\nUser ID: {message.author.id}"
+        await deleted_messages_channel.send(deleted_message)
 
-        # Add a reaction to the question message for easier answering
-        await question_msg.add_reaction("✉️")
+        # Delete the answer message
+        await message.delete()
 
-        # Wait for an answer
-        def check_answer(reaction, user):
-            return (
-                reaction.message.id == question_msg.id
-                and str(reaction.emoji) == "✉️"
-                and user != self.bot.user
-            )
+    @commands.command()
+    async def answer(self, ctx):
+        deleted_messages_channel = self.bot.get_channel(self.deleted_messages_channel_id)
+        answer_channel = self.bot.get_channel(self.answer_channel_id)
 
-        try:
-            reaction, user = await self.bot.wait_for("reaction_add", timeout=300.0, check=check_answer)
-            answer_content = reaction.message.content
+        # Fetch the most recent message from the deleted messages channel
+        async for message in deleted_messages_channel.history(limit=1):
+            if "Deleted Question" in message.content:
+                # Extract the question content and user ID from the deleted message
+                lines = message.content.split("\n")
+                content = lines[1].split(": ")[1]
+                user_id = lines[2].split(": ")[1]
 
-            # Create an embed with the answer and user information
-            answer_embed = discord.Embed(title="Answer", description=answer_content, color=discord.Color.green())
-            answer_embed.set_author(name=user.name, icon_url=user.avatar_url)
+                # Fetch the user who asked the question
+                user = await self.bot.fetch_user(user_id)
 
-            # Ping the user who asked the question and send the answer embed to the answer channel
-            answer_embed.set_footer(text=f"In response to {message.author.name}")
-            await answer_channel.send(f"<@{message.author.id}>", embed=answer_embed)
-        except asyncio.TimeoutError:
-            await question_channel.send("No answer received within 5 minutes.")
+                # Create an embed with the question, answer, and mention
+                embed = discord.Embed(title="Answer", color=discord.Color.green())
+                embed.add_field(name="Question", value=content)
+                embed.add_field(name="Answer", value=ctx.message.content)
+                embed.set_footer(text=f"In response to {user.name}")
+
+                # Send the answer embed to the answer channel
+                await answer_channel.send(f"<@{user.id}>", embed=embed)
+
+                # Notify the user who asked the question
+                await ctx.send(f"Answer sent to {user.mention} in {answer_channel.mention}.")
+                break
+
+
 
 async def setup(bot):
     await bot.add_cog(qna(bot))
