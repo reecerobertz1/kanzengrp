bbae66a0909ac05ca4637023c285b7666625bfb0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-28 20:19:46 +0100

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -5,12 +5,13 @@ class status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def status(self, ctx, *, status_text):
-        """Set the bot's status message."""
-        activity = discord.Game(name=status_text)
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.set_status()
+
+    async def set_status(self):
+        activity = Game(name="My prefix is +")
         await self.bot.change_presence(activity=activity)
-        await ctx.send(f"Status set to: {status_text}")
 
 async def setup(bot):
     await bot.add_cog(status(bot))
