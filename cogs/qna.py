95c9a2fedf9ee75b91b239e589024bd24e158e51
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-28 22:06:31 +0100

    Update qna.py

--- cogs/qna.py
+++ cogs/qna.py
@@ -5,10 +5,14 @@ from discord.ext import commands
 class qna(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.server1_deleted_messages_channel_id = 1123696501441450107
-        self.server1_answer_channel_id = 1123696762985656451
-        self.server2_deleted_messages_channel_id = 1123719751479341107
-        self.server2_answer_channel_id = 1123719714594639922
+        self.deleted_messages_channels = {
+            1123347338841313331: 1123696501441450107,  # Server 1
+            1122181605591621692: 1234567890  # Server 2
+        }
+        self.answer_channels = {
+            1123347338841313331: 1123696762985656451,  # Server 1
+            1122181605591621692: 2345678901  # Server 2
+        }
 
     @commands.command(hidden=True)
     async def answer(self, ctx, *, response: str):
@@ -18,40 +22,37 @@ class qna(commands.Cog):
             if "^" in msg.content:
                 question, asker = msg.content.split("^ ")
                 user = await self.bot.fetch_user(int(asker))
-                if ctx.guild.id == 1123347338841313331:  # Server 1
-                    answer_channel = self.bot.get_channel(self.server1_answer_channel_id)
-                elif ctx.guild.id == 1122181605591621692:  # Server 2
-                    answer_channel = self.bot.get_channel(self.server2_answer_channel_id)
-                else:
+                deleted_messages_channel_id = self.deleted_messages_channels.get(ctx.guild.id)
+                answer_channel_id = self.answer_channels.get(ctx.guild.id)
+
+                if deleted_messages_channel_id and answer_channel_id:
+                    deleted_messages_channel = self.bot.get_channel(deleted_messages_channel_id)
+                    answer_channel = self.bot.get_channel(answer_channel_id)
+
+                    embed = discord.Embed(title="kanzen q&n", color=0x2b2d31, description=f"**question:** {question}**\nanswer:** {response}")
+                    embed.set_footer(text=f"asked by {user.display_name} | answered by {ctx.author.display_name}")
+
+                    await answer_channel.send(user.mention)
+                    await answer_channel.send(embed=embed)
                     return
-
-                embed = discord.Embed(title="kanzen q&n", color=0x2b2d31, description=f"**question:** {question}**\nanswer:** {response}")
-                embed.set_footer(text=f"asked by {user.display_name} | answered by {ctx.author.display_name}")
-
-                await answer_channel.send(user.mention)
-                await answer_channel.send(embed=embed)
-                return
 
         await ctx.send("Failed to retrieve the question or the question format is incorrect.")
 
     @commands.command()
     async def qna(self, ctx, *, question: str):
-        if ctx.guild.id == 1123347338841313331:  # Server 1
-            deleted_messages_channel = self.bot.get_channel(self.server1_deleted_messages_channel_id)
-        elif ctx.guild.id == 1122181605591621692:  # Server 2
-            deleted_messages_channel = self.bot.get_channel(self.server2_deleted_messages_channel_id)
-        else:
-            return
+        deleted_messages_channel_id = self.deleted_messages_channels.get(ctx.guild.id)
 
-        q = await ctx.reply("Your question has been sent to the lead!")
+        if deleted_messages_channel_id:
+            deleted_messages_channel = self.bot.get_channel(deleted_messages_channel_id)
 
-        message = f"{question} ^ {ctx.author.id}"
-        await deleted_messages_channel.send(message)
+            q = await ctx.reply("Your question has been sent to the lead!")
 
-        await asyncio.sleep(3)
-        await ctx.message.delete()
-        await q.delete()
+            message = f"{question} ^ {ctx.author.id}"
+            await deleted_messages_channel.send(message)
 
+            await asyncio.sleep(3)
+            await ctx.message.delete()
+            await q.delete()
 
 
 
