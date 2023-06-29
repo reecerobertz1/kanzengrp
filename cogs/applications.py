001df2ae4735a9f4806c3e085d1c6760fc0101c1
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-29 16:34:17 +0100

    Update applications.py

--- cogs/applications.py
+++ cogs/applications.py
@@ -187,15 +187,15 @@ class applications(commands.Cog):
         if guild_id == 1122181605591621692:
             server_name = "Kanzen"
             decline_channel_id = 1123588044180684800
-            message = f"{member.mention}"
+            message = f"{member.mention} was declined"
         elif guild_id == 1123347338841313331:
             server_name = "Auragrp"
             decline_channel_id = 1123588246614577213
-            message = f"{member.mention}"
+            message = f"{member.mention} was declined"
         elif guild_id == 901409710572466217:
             server_name = "Daegutown"
             decline_channel_id = 901410829218492456
-            message = f"{member.mention}"
+            message = f"{member.mention} was declined"
         else:
             await ctx.reply("This command is not available in this server.")
             return
@@ -203,7 +203,7 @@ class applications(commands.Cog):
         decline_channel = self.bot.get_channel(decline_channel_id)
         if decline_channel:
             await decline_channel.send(message)
-            await ctx.send(f"")
+            await ctx.send(f" ** **")
         else:
             await ctx.reply(f"Decline message has been sent to {member.mention}.")
 
@@ -218,7 +218,7 @@ class applications(commands.Cog):
 
         try:
             await member.send(f"Hey, you have been declined in {server_name}. Please don't be upset or discouraged! We will have more recruitments in the future. <3")
-            await ctx.reply(f"Decline message sent to {member.mention} in {server_name}.")
+            await ctx.reply(f"Decline message sent to {member.mention}")
         except discord.Forbidden:
             await ctx.send("Failed to send the decline message. Please make sure the user has their DMs enabled.")
 
