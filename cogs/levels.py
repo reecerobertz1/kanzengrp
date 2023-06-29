8c0beb124f57d205f28afd2fe18bb1e35afd5b8c
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-29 22:44:21 +0100

    Update levels.py

--- cogs/levels.py
+++ cogs/levels.py
@@ -1,3 +1,4 @@
+import json
 import discord
 from discord.ext import commands
 from discord.ext.commands import BucketType
@@ -7,41 +8,40 @@ class levels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def get_level(self, xp):
+        return int((xp // 100) ** 0.5)
+
+    def save_data(self):
+        with open("xp_data.json", "w") as file:
+            json.dump(self.xp_data, file)
+
+    def load_data(self):
+        try:
+            with open("xp_data.json", "r") as file:
+                self.xp_data = json.load(file)
+        except FileNotFoundError:
+            self.xp_data = {}
+
+    def update_xp(self, guild_id, user_id, xp):
+        if guild_id not in self.xp_data:
+            self.xp_data[guild_id] = {}
+        self.xp_data[guild_id][user_id] = self.xp_data[guild_id].get(user_id, 0) + xp
+        self.save_data()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.load_data()
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot:
-            await self.add_experience(message.author, 5)
+        if message.author.bot:
+            return
 
-    async def add_experience(self, member, xp):
-        # Add the given amount of XP to the member's total
-        # You can customize this logic to suit your needs
-        # Example: Store XP in a database or file
-        # For now, we'll use a simple dictionary as an in-memory storage
-        guild_id = member.guild.id
-        user_id = member.id
+        guild_id = message.guild.id
+        user_id = message.author.id
+        xp = 10  # Modify this to change the XP gained per message
 
-        if guild_id not in self.bot.xp_data:
-            self.bot.xp_data[guild_id] = {}
-        if user_id not in self.bot.xp_data[guild_id]:
-            self.bot.xp_data[guild_id][user_id] = 0
-
-        self.bot.xp_data[guild_id][user_id] += xp
-
-        # Check if the member has leveled up
-        level = self.get_level(self.bot.xp_data[guild_id][user_id])
-        if level > 1:
-            await self.check_level_up(member, level)
-
-    def get_level(self, xp):
-        # Calculate the level based on the XP
-        # You can customize this logic to suit your needs
-        return xp // 100  # Assuming 100 XP per level
-
-    async def check_level_up(self, member, level):
-        # Perform actions when a member levels up
-        # You can customize this to your liking
-        # Example: Assign roles, send messages, etc.
-        await member.send(f"Congratulations! You leveled up to level {level}!")
+        self.update_xp(guild_id, user_id, xp)
 
     @commands.command()
     async def rank(self, ctx, member: discord.Member = None):
@@ -49,60 +49,55 @@ class levels(commands.Cog):
         guild_id = ctx.guild.id
         user_id = member.id
 
-        xp = self.bot.xp_data.get(guild_id, {}).get(user_id, 0)
+        xp = self.xp_data.get(guild_id, {}).get(user_id, 0)
         level = self.get_level(xp)
 
+        rank_card = discord.File("rank_card.png", filename="rank_card.png")
         embed = discord.Embed(title="Rank Card", color=discord.Color.blue())
         embed.set_author(name=member.display_name, icon_url=member.avatar_url)
         embed.set_thumbnail(url=member.avatar_url)
         embed.add_field(name="Level", value=str(level))
         embed.add_field(name="XP", value=str(xp))
 
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.is_owner()
-    async def addxp(self, ctx, member: discord.Member, xp: int):
-        await self.add_experience(member, xp)
-        await ctx.send(f"{xp} XP added to {member.display_name}.")
-
-    @commands.command()
-    @commands.is_owner()
-    async def removexp(self, ctx, member: discord.Member, xp: int):
-        guild_id = ctx.guild.id
-        user_id = member.id
-
-        if guild_id in self.bot.xp_data and user_id in self.bot.xp_data[guild_id]:
-            self.bot.xp_data[guild_id][user_id] = max(0, self.bot.xp_data[guild_id][user_id] - xp)
-            await ctx.send(f"{xp} XP removed from {member.display_name}.")
-        else:
-            await ctx.send(f"{member.display_name} has no XP data.")
+        await ctx.send(file=rank_card, embed=embed)
 
     @commands.command()
     async def leaderboard(self, ctx, page: int = 1):
         guild_id = ctx.guild.id
+        xp_data = self.xp_data.get(guild_id, {})
 
-        if guild_id in self.bot.xp_data:
-            xp_data = self.bot.xp_data[guild_id]
-            sorted_xp_data = sorted(xp_data.items(), key=lambda x: x[1], reverse=True)
+        sorted_users = sorted(xp_data, key=xp_data.get, reverse=True)
+        start_index = (page - 1) * 10
+        end_index = start_index + 10
+        leaderboard = ""
 
-            items_per_page = 10
-            start_index = (page - 1) * items_per_page
-            end_index = start_index + items_per_page
+        for index, user_id in enumerate(sorted_users[start_index:end_index], start=start_index):
+            member = ctx.guild.get_member(user_id)
+            if member:
+                leaderboard += f"{index + 1}. {member.display_name} - Level {self.get_level(xp_data[user_id])}\n"
 
-            leaderboard = []
-            for index, (user_id, xp) in enumerate(sorted_xp_data[start_index:end_index], start=start_index):
-                member = ctx.guild.get_member(user_id)
-                if member:
-                    leaderboard.append(f"{index + 1}. {member.display_name}: {xp} XP")
+        embed = discord.Embed(title="Leaderboard", description=leaderboard, color=discord.Color.blue())
+        embed.set_footer(text=f"Page {page}/{((len(sorted_users) - 1) // 10) + 1}")
 
-            if leaderboard:
-                embed = discord.Embed(title=f"Leaderboard (Page {page})", description="\n".join(leaderboard), color=discord.Color.gold())
-                await ctx.send(embed=embed)
-            else:
-                await ctx.send("No leaderboard data available.")
-        else:
-            await ctx.send("No leaderboard data available.")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def addxp(self, ctx, member: discord.Member, xp: int):
+        guild_id = ctx.guild.id
+        user_id = member.id
+
+        self.update_xp(guild_id, user_id, xp)
+        await ctx.send(f"Added {xp} XP to {member.display_name}.")
+
+    @commands.command()
+    @commands.has_permissions(manage_guild=True)
+    async def removexp(self, ctx, member: discord.Member, xp: int):
+        guild_id = ctx.guild.id
+        user_id = member.id
+
+        self.update_xp(guild_id, user_id, -xp)
+        await ctx.send(f"Removed {xp} XP from {member.display_name}.")
 
 
 async def setup(bot):
