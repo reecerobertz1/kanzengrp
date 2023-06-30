e5e2d29b9d3b2f4d4db7777f028e019ec0b1fd07
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-30 13:23:03 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -51,7 +51,7 @@ class welcandleave(commands.Cog):
             await channel.send(embed=embed)
             """DAEGU"""
         elif member.guild.id == self.server5_id:
-            embed = discord.Embed(title=f'<:brazy_milksip:958479364184490075> : {member.name} has joined the server!', color=0x2b2d31, description=f"• Make sure you read our [rules](https://discord.com/channels/1123347338841313331/1123351779158016060)\n• Go and get your [roles](https://discord.com/channels/1123347338841313331/1123351943549562950)\n• apply [here!](https://discord.com/channels/1123347338841313331/1123352172143329331)\n• need help? ping <@&1123361291113865226>")
+            embed = discord.Embed(title=f'<:brazy_milksip:958479364184490075> : {member.name} has joined the server!', color=0x2b2d31, description=f"• Make sure you read our [server info](https://discord.com/channels/896619762354892821/896924214311911476)\n• Go and get your [roles](https://discord.com/channels/896619762354892821/1062169874748686386) + [biases](https://discord.com/channels/896619762354892821/1044478054258458685)\n\n• need help? ping <@&896881532671754260>, <@1119948686550630410> or <@903955975000698922>")
             embed.set_footer(text='Have fun! Thank you for joining', icon_url=member.display_avatar.url)
             channel = self.bot.get_channel(self.server5_channel)
             embed.set_thumbnail(url=member.display_avatar.url)
