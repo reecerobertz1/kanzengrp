78e8e2fabeef85754a630f49c153164f78202f8e
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-06-30 13:20:24 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -13,6 +13,8 @@ class welcandleave(commands.Cog):
         self.server3_channel = 1123347339797594144
         self.server4_id = 1122181605591621692
         self.server4_channel = 1123334440727367730
+        self.server5_id = 896619762354892821
+        self.server5_channel = 896924663819694132
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
@@ -44,6 +46,14 @@ class welcandleave(commands.Cog):
             embed = discord.Embed(title=f'<:brazy_milksip:958479364184490075> : {member.name} has joined the server!', color=0x2b2d31, description=f"• Make sure you read our [rules](https://discord.com/channels/1123347338841313331/1123351779158016060)\n• Go and get your [roles](https://discord.com/channels/1123347338841313331/1123351943549562950)\n• apply [here!](https://discord.com/channels/1123347338841313331/1123352172143329331)\n• need help? ping <@&1123361291113865226>")
             embed.set_footer(text='Have fun! Thank you for joining', icon_url=member.display_avatar.url)
             channel = self.bot.get_channel(self.server4_channel)
+            embed.set_thumbnail(url=member.display_avatar.url)
+            await channel.send(f'{member.mention}')
+            await channel.send(embed=embed)
+            """DAEGU"""
+        elif member.guild.id == self.server5_id:
+            embed = discord.Embed(title=f'<:brazy_milksip:958479364184490075> : {member.name} has joined the server!', color=0x2b2d31, description=f"• Make sure you read our [rules](https://discord.com/channels/1123347338841313331/1123351779158016060)\n• Go and get your [roles](https://discord.com/channels/1123347338841313331/1123351943549562950)\n• apply [here!](https://discord.com/channels/1123347338841313331/1123352172143329331)\n• need help? ping <@&1123361291113865226>")
+            embed.set_footer(text='Have fun! Thank you for joining', icon_url=member.display_avatar.url)
+            channel = self.bot.get_channel(self.server5_channel)
             embed.set_thumbnail(url=member.display_avatar.url)
             await channel.send(f'{member.mention}')
             await channel.send(embed=embed)
