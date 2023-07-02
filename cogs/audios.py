8daa16fcd009e0604bb482c0ffa95c3a01690e3c
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-02 19:00:29 +0100

    Update audios.py

--- cogs/audios.py
+++ cogs/audios.py
@@ -6,7 +6,6 @@ from discord.ext import commands
 class audios(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot = bot
         self.audio_links = ['https://on.soundcloud.com/WwHQzqBHCafrdgLe6']
 
     @commands.command()
