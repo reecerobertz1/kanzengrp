31d2e042cef2a1d501329acbcb3739a8bb261ef3
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-03 04:49:36 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -107,7 +107,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def helpjail(self, ctx):
-        embed = discord.Embed(Title='+jail <member>', description='Put someone or yourself in jail!', color=0x2b2d31)
+        embed = discord.Embed(title='+jail <member>', description='Put someone or yourself in jail!', color=0x2b2d31)
         embed.add_field(name='aliases', value='prison, lockup', inline=False)
         embed.add_field(name='arguments', value='`member`\n<:reply:1125269313432059904> mention someone to put in jail\nor dont mention someone to put yourself in jail', inline=False)
         embed.add_field(name='example', value='+jail <@609515684740988959>', inline=False)
