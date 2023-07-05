a79edeb51aa6fd86a7afecf370302ef46ab61963
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-05 06:16:11 +0100

    Update collectables.py

--- cogs/collectables.py
+++ cogs/collectables.py
@@ -9,25 +9,29 @@ class Unlock(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def get_unlock_data(self):
+    def get_unlock_data(self, guild_id):
+        if guild_id not in self.unlock_data:
+            self.unlock_data[guild_id] = {}
+        return self.unlock_data[guild_id]
+
+    def save_unlock_data(self):
+        with open("unlock_data.json", "w") as file:
+            json.dump(self.unlock_data, file, indent=4)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
         try:
             with open("unlock_data.json", "r") as file:
-                data = json.load(file)
+                self.unlock_data = json.load(file)
         except FileNotFoundError:
-            data = {}
-        return data
-
-    def save_unlock_data(self, data):
-        with open("unlock_data.json", "w") as file:
-            json.dump(data, file, indent=4)
+            self.unlock_data = {}
 
     @commands.command()
     async def unlock(self, ctx):
-        server_id = 1121841073673736215
-        channel_id = 1125999933149949982
+        guild_id = 1121841073673736215
         member = ctx.author
 
-        if ctx.guild.id != server_id:
+        if ctx.guild.id != guild_id:
             await ctx.send("This command can only be used in the specified server.")
             return
 
@@ -69,29 +73,30 @@ class Unlock(commands.Cog):
         if unlock_level in ["rare", "epic", "legendary"]:
             xp = unlock_data["xp"]
             xp_message = unlock_data["message"]
-            unlock_channel = self.bot.get_channel(channel_id)
+            unlock_channel_id = 1125999933149949982  # ID of the unlock channel
+            unlock_channel = self.bot.get_channel(unlock_channel_id)
             await unlock_channel.send(f"{member.mention} has found {xp} XP!")
 
             if unlock_level == "legendary":
                 embed = discord.Embed(
                     title="Legendary Unlock",
-                    description="Congratulations on unlocking a legendary item!\nPlease don't share this with anyone else in the server.",
+                    description="Congratulations on unlocking a legendary item!\n\nPlease don't share this with anyone else in the server.",
                     color=discord.Color.gold()
                 )
-                embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
                 await member.send(embed=embed)
+                await unlock_channel.send(f"{member.mention} has unlocked 2000 XP!")
             else:
                 await ctx.send(f"{xp_message} {random.choice(unlock_data['emojis'])}")
 
-        if unlock_level in ["common", "uncommon"]:
+        elif unlock_level in ["common", "uncommon"]:
             xp_message = unlock_data["message"]
             await ctx.send(f"{xp_message} {random.choice(unlock_data['emojis'])}")
 
-        data = self.get_unlock_data()
-        if str(member.id) not in data:
-            data[str(member.id)] = []
-        data[str(member.id)].append(unlock_level)
-        self.save_unlock_data(data)
+        guild_unlock_data = self.get_unlock_data(ctx.guild.id)
+        if str(member.id) not in guild_unlock_data:
+            guild_unlock_data[str(member.id)] = []
+        guild_unlock_data[str(member.id)].append(unlock_level)
+        self.save_unlock_data()
 
     @commands.command()
     async def unlocked(self, ctx):
@@ -109,7 +114,6 @@ class Unlock(commands.Cog):
             await ctx.send(f"{member.mention}, you have unlocked the following items: {unlocked_items}")
         else:
             await ctx.send(f"{member.mention}, you have not unlocked any items yet.")
-
 
 
 async def setup(bot):
