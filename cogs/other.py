b77c0bb7b170e1a95d9e43eaa3c7ebd6c1e2d8ce
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-07 23:39:11 +0100

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -16,5 +16,15 @@ class other(commands.Cog):
         embed.set_footer(text='If you have any question, do +qna')
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def randomembed2(self, ctx):
+        embed = discord.Embed(
+            title='> Kanzen Q&A',
+            color=0x2b2d31)
+        embed.set_image(url='https://cdn.discordapp.com/attachments/1122181606254330008/1127005297270071417/jisoo_and_rose_photo_00000.png')
+        embed.add_field(name='how to apply', value="<a:bounceyarrow:1126865547255091343> use the command `+qna` to ask the lead a question.\n<a:bounceyarrow:1126865547255091343> your question will be sent to a separate channel and when the lead answers, the answer will be sent to <#1123696762985656451>.\n<a:bounceyarrow:1126865547255091343> you will also be pinged when your question has been answered.\n<a:bounceyarrow:1126865547255091343> please do not abuse this command and spam! you will be kicked from the server, and banned if you continue", inline=False)
+        embed.set_footer(text='If you have any question, do +qna')
+        await ctx.send(embed=embed)
+
 async def setup(bot):
     await bot.add_cog(other(bot))
