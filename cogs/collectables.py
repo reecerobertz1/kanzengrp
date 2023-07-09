06817d3e121dad98f43ab60be6f99177fd922f91
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-09 23:21:50 +0100

    Update collectables.py

--- cogs/collectables.py
+++ cogs/collectables.py
@@ -16,6 +16,18 @@ class Unlock(commands.Cog):
         rancoins= random.choice(coins)
         await ctx.reply(f'You have received <:coin:1127703632389865682> {rancoins}! Come back tomorrow to get more')
 
+    @commands.command()
+    async def xpshop(self, ctx):
+        embed = discord.Embed(title='XP Shop',
+                              description='Use the command `+buy (XP amount) to buy XP!',
+                              color=0x2b2d31)
+        embed.add_field(name='<:coin:1127703632389865682> 350', value='500xp', inline=False)
+        embed.add_field(name='<:coin:1127703632389865682> 500', value='750xp', inline=False)
+        embed.add_field(name='<:coin:1127703632389865682> 750', value='1,000xp', inline=False)
+        embed.add_field(name='<:coin:1127703632389865682> 1,000', value='1,250xp', inline=False)
+        embed.add_field(name='<:coin:1127703632389865682> 1,250', value='1,500xp', inline=False)
+        await ctx.reply(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Unlock(bot))
