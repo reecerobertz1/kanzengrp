b9f3c9fc6002a5753cb38f3f0f89aace454890f3
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-10 01:50:09 +0100

    Update collectables.py

--- cogs/collectables.py
+++ cogs/collectables.py
@@ -10,22 +10,44 @@ class Unlock(commands.Cog):
         self.bot = bot
         self.unlocked_items = {}
 
+def get_user_coins(user_id):
+    coins_dict = {
+        1234567890: 500,  # Example user ID and coin balance
+        # Add more user ID and coin balance pairs as needed
+    }
+    return coins_dict.get(user_id, 0)  # Return the user's coin balance, or 0 if not found
+
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
     @commands.command()
     async def daily(self, ctx):
-        coins = ['150', '250', '300', '350', '400', '450', '500', '550', '600', '650', '700', '750', '800', '850', '900', '950', '1,000']
-        rancoins= random.choice(coins)
-        await ctx.reply(f'You have received <:coin:1127703632389865682> {rancoins}! Come back tomorrow to get more')
+        coins = random.randint(1, 1000)
+        xp = random.randint(1, 500)
+        await ctx.send(f"You found {coins} coins!")
+
+        if random.random() < 0.5:
+            channel = self.bot.get_channel(1125999933149949982)
+            await channel.send(f"{ctx.author.mention} found {xp} XP!")
 
     @commands.command()
-    async def xpshop(self, ctx):
-        embed = discord.Embed(title='XP Shop', description='Use the command `+buy (XP amount) to buy XP!', color=0x2b2d31)
-        embed.add_field(name='<:coin:1127703632389865682> 350', value='500xp', inline=False)
-        embed.add_field(name='<:coin:1127703632389865682> 500', value='750xp', inline=False)
-        embed.add_field(name='<:coin:1127703632389865682> 750', value='1,000xp', inline=False)
-        embed.add_field(name='<:coin:1127703632389865682> 1,000', value='1,250xp', inline=False)
-        embed.add_field(name='<:coin:1127703632389865682> 1,250', value='1,500xp', inline=False)
-        await ctx.reply(embed=embed)
+    async def shop(self, ctx):
+        embed = discord.Embed(title="Shop", description="Welcome to the shop!")
+        embed.add_field(name="XP", value="Price: 100 coins", inline=False)
+        embed.add_field(name="Emoji", value="Price: 200 coins", inline=False)
+        await ctx.send(embed=embed)
 
+    @commands.command()
+    async def buy(self, ctx, item: str):
+        if item.lower() == "xp":
+            coins = 100
+            if coins <= get_user_coins(ctx.author.id):
+                # Deduct coins from user's balance
+                # Perform other logic for buying XP
+                await ctx.send(f"{ctx.author.mention} bought XP!")
+            else:
+                await ctx.send("Insufficient coins!")
 
 async def setup(bot):
     await bot.add_cog(Unlock(bot))
