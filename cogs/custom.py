6492c47bd2c68ac043a1e5fe14ffcdb1b4b84537
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-15 21:08:14 +0100

    Update custom.py

--- cogs/custom.py
+++ cogs/custom.py
@@ -1,4 +1,5 @@
 import json
+import discord
 from discord.ext import commands
 
 class custom(commands.Cog):
@@ -66,6 +67,31 @@ class custom(commands.Cog):
         else:
             await ctx.send(f"The custom command `{command_name}` does not exist.")
 
+    @commands.command()
+    async def listcmds(self, ctx):
+        server_id = str(ctx.guild.id)
+        custom_commands = {}
+
+        try:
+            with open("custom.json", "r") as file:
+                custom_commands = json.load(file)
+        except FileNotFoundError:
+            pass
+
+        if server_id in custom_commands:
+            embed = discord.Embed(title="Custom Commands", color=0x2b2d31)
+            for command_name, command_data in custom_commands[server_id].items():
+                user_id = command_data["user_id"]
+                response = command_data["response"]
+
+                user = self.bot.get_user(user_id)
+                user_mention = user.mention if user else f"Unknown User ({user_id})"
+
+                embed.add_field(name=f"Command: {command_name}", value=f"Response: {response}\nAdded by: {user_mention}", inline=False)
+
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("There are no custom commands.")
 
 
 async def setup(bot):
