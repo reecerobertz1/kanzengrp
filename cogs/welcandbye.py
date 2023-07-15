9a858c5472e97705f33115c62fb76d6548c2fa9b
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-15 16:29:35 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -98,7 +98,6 @@ class welcandleave(commands.Cog):
                 await channel.send(embed=embed)
 
     @commands.command()
-    @commands.has_permissions(manage_guild=True)
     async def perks(self, ctx):
         server_id = ctx.guild.id
 
