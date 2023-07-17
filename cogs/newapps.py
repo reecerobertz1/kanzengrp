a4cb806f30fdbbee05f1bdea32fdc8232d50af47
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 17:05:39 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -18,12 +18,12 @@ class Slash(commands.Cog):
     async def test(self, interaction: discord.Interaction):
         await interaction.response.send_message('testing')
 
-    @app_commands.command(name='logos', description='Get Kanzen logos')
+    @app_commands.command(name='kanzen', description='Get Kanzen logos')
     @app_commands.guilds(discord.Object(id=1121841073673736215))
     async def kanzenlogos(self, interaction: discord.Interaction):
         await interaction.response.send_message('https://mega.nz/folder/J40zCTYY#L73pTeQKWpCh15wpuQaIFA', ephemeral=True)
 
-    @app_commands.command(name='logos', description='Get Aura logos')
+    @app_commands.command(name='aura', description='Get Aura logos')
     @app_commands.guilds(discord.Object(id=957987670787764224))
     async def kanzenlogos(self, interaction: discord.Interaction):
         await interaction.response.send_message('https://mega.nz/folder/SNkySBBb#kNViVZOVnHzEFmFsuhtLOQ', ephemeral=True)
@@ -39,6 +39,12 @@ class testmodal(ui.Modal, title='Testing it'):
      test1 = ui.TextInput(label='Testing modals', placeholder="you're ugly...", style=discord.TextStyle.short)
      test2 = ui.TextInput(label='Testing modals', placeholder="you make me wanna die...", style=discord.TextStyle.long)
      test3 = ui.TextInput(label='Testing modals', placeholder="lol jk....", style=discord.TextStyle.short)
+     async def on_submit(self, interaction: discord.Interaction):
+          await interaction.response.send_message(f"{self.test1} {self.test2} {self.test3}", ephemeral=True)
+
+class ia(ui.Modal, title='Inactivity Message'):
+     test1 = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
+     test2 = ui.TextInput(label='Inactivity Reason', placeholder="", style=discord.TextStyle.long)
      async def on_submit(self, interaction: discord.Interaction):
           await interaction.response.send_message(f"{self.test1} {self.test2} {self.test3}", ephemeral=True)
 
