8c4e9f4065fb1d997a3b38c75c37b3a2b7bfc3fa
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 16:36:24 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from discord import ui
 
 class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+# slash commands
 
     @app_commands.command(name='test', description='testing slash commands')
     @app_commands.guilds(discord.Object(id=1121841073673736215))
@@ -20,15 +23,14 @@ class Slash(commands.Cog):
     async def test(self, interaction: discord.Interaction):
          await interaction.response.send_modal(testmodal())
 
+# modals
 
 class testmodal(discord.ui.Modal, title='Testing it'):
-     test1 = discord.ui.TextImput(label='Testing modals', placeholder="you're ugly...", style=discord.TextStyle.short)
-     test2 = discord.ui.TextImput(label='Testing modals', placeholder="you make me wanna die...", style=discord.TextStyle.long)
-     test3 = discord.ui.TextImput(label='Testing modals', placeholder="lol jk....", style=discord.TextStyle.short)
+     test1 = ui.TextImput(label='Testing modals', placeholder="you're ugly...", style=discord.TextStyle.short)
+     test2 = ui.TextImput(label='Testing modals', placeholder="you make me wanna die...", style=discord.TextStyle.long)
+     test3 = ui.TextImput(label='Testing modals', placeholder="lol jk....", style=discord.TextStyle.short)
      async def on_submit(self, interaction: discord.Interaction):
-          await interaction.response.send_message('testing modals')
-
-          
+          await interaction.response.send_message("lol modal hahahaha")
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Slash(bot))
