0eb937d5dfb2acb4a55c6134af4bf2fd8477f162
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 16:03:17 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -9,7 +9,7 @@ class Slash(commands.Cog):
     @app_commands.command(name='test', description='testing slash commands')
     @app_commands.guilds(discord.Object(id=1121841073673736215))
     async def test(self, interaction: discord.Interaction):
-        await interaction.responce.send_message('testing')
+        await interaction.response.send_message('testing')
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Slash(bot))
