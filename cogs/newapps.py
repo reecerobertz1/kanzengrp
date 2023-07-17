5363eed685aaa67c5c9d49ead912dfa9a6b512ed
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 19:03:01 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -57,7 +57,6 @@ class ia(ui.Modal, title='Inactivity Message'):
 		embed.add_field(name='Instagram Name:', value=f'{self.instagram}', inline=False)
 		embed.add_field(name="Account Link", value=f"https://instagram.com/{self.instagram.value}")
 		embed.add_field(name='Inactivity Reason:', value=f'{self.reason}', inline=False)
-		embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
 		await interaction.client.get_channel(id=1121913672822968330).send(embed=embed)
 		await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
                 
