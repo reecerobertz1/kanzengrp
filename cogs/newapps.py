adb1a6e80310196b3aa6648c4dda170943595848
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 19:04:54 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -49,20 +49,18 @@ class testmodal(ui.Modal, title='Testing it'):
           await interaction.response.send_message(f"https://instagram.com/{self.test1}")
 
 class ia(ui.Modal, title='Inactivity Message'):
-	instagram = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
-	reason = ui.TextInput(label='Inactivity Reason', placeholder="Enter your inactivity reason here...", style=discord.TextStyle.long)
-	async def on_submit(self, interaction: discord.Interaction):
-		await interaction.response.defer()
-		embed = discord.Embed(title='Inactivity Message', color=0x2b2d31)
-		embed.add_field(name='Instagram Name:', value=f'{self.instagram}', inline=False)
-		embed.add_field(name="Account Link", value=f"https://instagram.com/{self.instagram.value}")
-		embed.add_field(name='Inactivity Reason:', value=f'{self.reason}', inline=False)
-		await interaction.client.get_channel(id=1121913672822968330).send(embed=embed)
-		await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
-                
-	async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-		await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
-		traceback.print_tb(error.__traceback__)
+     instagram = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
+     reason = ui.TextInput(label='Inactivity Reason', placeholder="", style=discord.TextStyle.long)
+     async def on_submit(self, interaction: discord.Interaction):
+          await interaction.response.defer()
+          embed = discord.Embed(title='Inactivity Message', color=0x2b2d31)
+          embed.add_field(name='Instagram Name:', value=f'{self.instagram.value}', inline=False)
+          embed.add_field(name='Instagram Account Link:', value=f'https://instagram.com/{self.instagram.value}', inline=False)
+          embed.add_field(name='Inactivity Reason:', value=f'{self.reason.value}', inline=False)
+          embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
+          channel = await interaction.client.get_channel(id=1121913672822968330)
+          await channel.send(embed=embed)
+          await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Slash(bot))
