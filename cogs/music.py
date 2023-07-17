03ef7e3b1820dfeefba9d7dad711f2d359cfa4dd
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 03:17:40 +0100

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -9,9 +9,9 @@ class Music(commands.Cog):
     @commands.command(name='play_song', help='To play a song')
     async def play(self, ctx, url):
         server = ctx.guild
-        voice_channel = server.voice_client
+        voice_client = server.voice_client
         async with ctx.typing():
-            voice_channel.play(discord.FFmpegPCMAudio(url))
+            voice_client.play(discord.FFmpegPCMAudio(url))
         await ctx.send('Now playing: {}'.format(url))
 
     @commands.command(name='join', help='Tells the bot to join the voice channel')
@@ -21,7 +21,7 @@ class Music(commands.Cog):
             return
 
         voice_channel = ctx.author.voice.channel
-        voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
 
         if voice_client and voice_client.is_connected():
             await voice_client.move_to(voice_channel)
