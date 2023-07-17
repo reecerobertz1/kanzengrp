ef4e44d1d9b53dc358b0931b95568ba22d6f28c0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 01:45:35 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -11,8 +11,11 @@ class ia(ui.Modal, title='Inactivity Message'):
     instagram = ui.TextInput(label='What is your instagram username?', placeholder='Enter your instagram username here...', style=discord.TextStyle.short)
     iareason = ui.TextInput(label='What is your inactivity reason?', placeholder='Enter your reason here...', style=discord.TextStyle.long)
 
+    async def on_submit(interation: discord.Interation):
+        await interation.responce.send_message('boobs')
 
-    @commands.tree.command()
+
+    @commands.command()
     async def ianewtest(self, interation: discord.Integration):
         await interation.responce.send_modal(ia())
 
