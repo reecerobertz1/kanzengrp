fe5b344af24e716b0f706b129d8c87dd8376b9b2
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 01:36:33 +0100

    Create newapps.py

---
+++ cogs/newapps.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from colorama import Back, Fore, Style
+import time
+import json
+from discord import ui
+
+class modals(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+class ia(ui.Modal):
+    instagram = ui.TextInput(label='What is your instagram username?', placeholder='Enter your instagram username here...', style=discord.TextStyle.short)
+    iareason = ui.TextInput(label='What is your inactivity reason?', placeholder='Enter your reason here...', style=discord.TextStyle.long)
+
+
+    @commands.command()
+    async def ia(self, interation: discord.Integration):
+        await interation.responce.send_modal(ia())
+
+async def setup(bot):
+    await bot.add_cog(modals(bot))
