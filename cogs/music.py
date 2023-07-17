0b58738e2b3e68e74973ee1666540463caba21d3
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 03:52:25 +0100

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -18,13 +18,14 @@ class Music(commands.Cog):
     async def join(self, ctx):
         channel_id = 1055799905978957854  # Replace with your desired voice channel ID
 
-        voice_channel = self.bot.get_channel(channel_id)
+        voice_channel = discord.utils.get(ctx.guild.voice_channels, id=channel_id)
 
         if voice_channel:
-            voice_client = ctx.guild.voice_client
+            voice_client = ctx.voice_client
             if voice_client is not None:
-                await voice_client.disconnect()
-            await voice_channel.connect()
+                await voice_client.move_to(voice_channel)
+            else:
+                await voice_channel.connect()
             await ctx.send(f"Joined {voice_channel.name}")
         else:
             await ctx.send("Unable to find the voice channel.")
