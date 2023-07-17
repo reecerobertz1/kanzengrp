7b278661be6eed2b320d13c737cfb0afd7163def
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 16:32:41 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -15,5 +15,20 @@ class Slash(commands.Cog):
     async def test(self, interaction: discord.Interaction):
         await interaction.response.send_message('testing')
 
+
+    @app_commands.command(name='modal', description='open testing modal')
+    async def test(self, interaction: discord.Interaction):
+         await interaction.response.send_modal(testmodal())
+
+
+class testmodal(discord.ui.Modal, title='Testing it'):
+     test1 = discord.ui.TextImput(label='Testing modals', placeholder="you're ugly...", style=discord.TextStyle.short)
+     test2 = discord.ui.TextImput(label='Testing modals', placeholder="you make me wanna die...", style=discord.TextStyle.long)
+     test3 = discord.ui.TextImput(label='Testing modals', placeholder="lol jk....", style=discord.TextStyle.short)
+     async def on_submit(self, interaction: discord.Interaction):
+          await interaction.response.send_message('testing modals')
+
+          
+
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Slash(bot))
