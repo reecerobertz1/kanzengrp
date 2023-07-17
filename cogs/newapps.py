419918376254e32d47f5b1b24f625e6a9d803581
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 19:57:28 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -39,6 +39,11 @@ class Slash(commands.Cog):
     async def ia(self, interaction: discord.Interaction):
          await interaction.response.send_modal(ia())
 
+    @app_commands.command(name='apply', description='Apply for kanzen!')
+    @app_commands.guilds(discord.Object(id=1122181605591621692))
+    async def ia(self, interaction: discord.Interaction):
+         await interaction.response.send_modal(ia())
+
 # modals
 
 class testmodal(ui.Modal, title='Testing it'):
@@ -59,6 +64,23 @@ class ia(ui.Modal, title='Inactivity Message'):
           embed.add_field(name='Inactivity Reason:', value=f'{self.reason.value}', inline=False)
           embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
           channel = interaction.client.get_channel(1121913672822968330)
+          await channel.send(embed=embed)
+          await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
+
+class apps(ui.Modal, title='Kanzengrp Applications'):
+     instagram = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
+     discord = ui.TextInput(label='Discord username', placeholder="Enter your Discord username here...", style=discord.TextStyle.short)
+     edit = ui.TextInput(label='Edit link (Instagran or Streamable)', placeholder="Paste link here...", style=discord.TextStyle.short)
+     app = ui.TextInput(label='What app do you use for editing', placeholder="Editing app name...", style=discord.TextStyle.short)
+     extra = ui.TextInput(label='Anything else you would like us to know?', placeholder="Extra info here...", style=discord.TextStyle.long, required=False)
+     async def on_submit(self, interaction: discord.Interaction):
+          await interaction.response.defer()
+          embed = discord.Embed(title='Inactivity Message', color=0x2b2d31)
+          embed.add_field(name='Instagram Name:', value=f'{self.instagram.value}', inline=False)
+          embed.add_field(name='Instagram Account Link:', value=f'https://instagram.com/{self.instagram.value}', inline=False)
+          embed.add_field(name='Inactivity Reason:', value=f'{self.reason.value}', inline=False)
+          embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
+          channel = interaction.client.get_channel(1122183100038905908)
           await channel.send(embed=embed)
           await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
 
