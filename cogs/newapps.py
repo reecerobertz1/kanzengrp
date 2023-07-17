38f4c3227fac4e29d3011fb27f883b86c246d6d0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 15:41:16 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -1,18 +1,15 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-import traceback
-from typing import Optional
-from io import BytesIO
-from discord_slash import cog_ext
 
-class newapps(commands.Cog):
+class Slash(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name='test', description='testing')
-    async def test(self, interaction: discord.Interaction, name: str, age: int) -> None:
-         await interaction.response.send_message(f'My name is {name} and i am {age} years old!')
+        @app_commands.command(name='Test', description='testing slash commands')
+        @app_commands.guilds(discord.Object(id=1121841073673736215))
+        async def test(self, interaction: discord.Interaction):
+            await interaction.responce.send_message('testing')
 
 async def setup(bot: commands.Bot) -> None:
-	await bot.add_cog(newapps(bot))
+	await bot.add_cog(Slash(bot))
