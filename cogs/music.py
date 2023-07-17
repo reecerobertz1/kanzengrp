165b7be6ddc2cc6fd23462a23f96f9e12f1f5ebd
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-17 03:10:35 +0100

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,102 +1,92 @@
-"""import discord
-from discord.ext import commands
-from youtube_dl import YoutubeDL
-from spotdl.search.songObj import SongObj
-from spotdl.search.spotifyClient import SpotifyClient
-from spotdl.download.downloader import download_song
 import asyncio
+import discord
+from discord.ext import commands
+import youtube_dl
+
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(name='play_song', help='To play a song')
     async def play(self, ctx, url):
-        if not ctx.message.author.voice:
-            await ctx.send("You are not connected to a voice channel...")
+        server = ctx.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await self.ytdl_source(url)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="C:\\Users\\abhisar.ahuja\\Documents\\ffmpeg\\ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+
+    async def ytdl_source(self, url):
+        data = await self.bot.loop.run_in_executor(None, lambda: self.ytdl.extract_info(url, download=False))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title']
+        return filename
+
+    @commands.command(name='join', help='Tells the bot to join the voice channel')
+    async def join(self, ctx):
+        if not ctx.author.voice:
+            await ctx.send("{} is not connected to a voice channel".format(ctx.author.name))
             return
-
-        channel = ctx.message.author.voice.channel
-        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-
-        if voice_client and voice_client.is_connected():
-            await voice_client.move_to(channel)
         else:
-            voice_client = await channel.connect()
+            channel = ctx.author.voice.channel
+        await channel.connect()
 
-        if "soundcloud.com" in url:
-            source = await self.get_soundcloud_source(url)
-        elif "spotify.com" in url:
-            source = await self.get_spotify_source(url)
-        else:
-            await ctx.send("Invalid link provided...")
-            return
-
-        voice_client.play(source)
-
-    async def get_soundcloud_source(self, url):
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-
-        with YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, method='fallback')
-
-        return source
-
-    async def get_spotify_source(self, url):
-        spotify_client = SpotifyClient()
-        song_obj = SongObj(url)
-
-        await spotify_client.build_song(song_obj)
-
-        try:
-            download_song(song_obj)
-        except Exception as e:
-            print(f"An error occurred while downloading the song: {e}")
-            return None
-
-        source = await discord.FFmpegOpusAudio.from_probe(song_obj.file_path, method='fallback')
-
-        return source
-
-    @commands.command()
+    @commands.command(name='pause', help='This command pauses the song')
     async def pause(self, ctx):
-        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-
-        if voice_client and voice_client.is_playing():
-            voice_client.pause()
-            await ctx.send("Playback paused.")
+        voice_client = ctx.guild.voice_client
+        if voice_client.is_playing():
+            await voice_client.pause()
         else:
-            await ctx.send("No audio is currently playing.")
+            await ctx.send("The bot is not playing anything at the moment.")
 
-    @commands.command()
+    @commands.command(name='resume', help='Resumes the song')
+    async def resume(self, ctx):
+        voice_client = ctx.guild.voice_client
+        if voice_client.is_paused():
+            await voice_client.resume()
+        else:
+            await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+    @commands.command(name='leave', help='To make the bot leave the voice channel')
+    async def leave(self, ctx):
+        voice_client = ctx.guild.voice_client
+        if voice_client.is_connected():
+            await voice_client.disconnect()
+        else:
+            await ctx.send("The bot is not connected to a voice channel.")
+
+    @commands.command(name='stop', help='Stops the song')
     async def stop(self, ctx):
-        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-
-        if voice_client and voice_client.is_playing():
-            voice_client.stop()
-            await ctx.send("Playback stopped.")
+        voice_client = ctx.guild.voice_client
+        if voice_client.is_playing():
+            await voice_client.stop()
         else:
-            await ctx.send("No audio is currently playing.")
+            await ctx.send("The bot is not playing anything at the moment.")
 
-    @commands.command()
-    async def queue(self, ctx):
-        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-
-        if voice_client and voice_client.is_playing():
-            queue = voice_client.source
-            await ctx.send(f"Current queue: {queue}")
-        else:
-            await ctx.send("No audio is currently playing.")
-
-
-async def setup(bot):
-    await bot.add_cog(Music(bot))"""
+async def setup(bot: commands.Bot) -> None:
+	await bot.add_cog(Music(bot))
