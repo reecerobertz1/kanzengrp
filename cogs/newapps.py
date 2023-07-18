77b1c5e39338d03f6e7ac641bea46e2e14d812c8
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-18 17:45:15 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -44,6 +44,11 @@ class Slash(commands.Cog):
     async def apps(self, interaction: discord.Interaction):
          await interaction.response.send_modal(apps())
 
+    @app_commands.command(name='apply', description='Apply for aura!')
+    @app_commands.guilds(discord.Object(id=1122181605591621692))
+    async def auraapps(self, interaction: discord.Interaction):
+         await interaction.response.send_modal(auraapps())
+
 # modals
 
 class testmodal(ui.Modal, title='Testing it'):
@@ -67,6 +72,9 @@ class ia(ui.Modal, title='Inactivity Message'):
           await channel.send(embed=embed)
           await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
 
+# kanzen apps
+
+
 class apps(ui.Modal, title='Kanzengrp Applications'):
      instagram = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
      dcname = ui.TextInput(label='Discord username', placeholder="Enter your Discord username here...", style=discord.TextStyle.short)
@@ -87,7 +95,27 @@ class apps(ui.Modal, title='Kanzengrp Applications'):
           await channel.send(embed=embed)
           await interaction.followup.send(f'Your application has been sent successfully', ephemeral=True)
 
-    # normal commands
+    # aura apps
+
+class auraapps(ui.Modal, title='Auragrp Applications'):
+     instagram = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
+     dcname = ui.TextInput(label='Discord username', placeholder="Enter your Discord username here...", style=discord.TextStyle.short)
+     edit = ui.TextInput(label='Edit link (Instagran or Streamable)', placeholder="Paste link here...", style=discord.TextStyle.short)
+     app = ui.TextInput(label='What app do you use for editing', placeholder="Editing app name...", style=discord.TextStyle.short)
+     extra = ui.TextInput(label='Anything else you would like us to know?', placeholder="Extra info here...", style=discord.TextStyle.paragraph, required=False)
+     async def on_submit(self, interaction: discord.Interaction):
+          await interaction.response.defer()
+          embed = discord.Embed(title='Aura Forms', color=0x2b2d31)
+          embed.add_field(name='Instagram Name:', value=f'{self.instagram.value}', inline=False)
+          embed.add_field(name='Instagram Account Link:', value=f'https://instagram.com/{self.instagram.value}', inline=False)
+          embed.add_field(name='Discord Username:', value=f'{self.dcname.value}', inline=False)
+          embed.add_field(name='Edit:', value=f'{self.edit.value}', inline=False)
+          embed.add_field(name='Editing app:', value=f'{self.app.value}', inline=False)
+          embed.add_field(name='Anything else:', value=f'{self.extra.value}', inline=False)
+          embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
+          channel = interaction.client.get_channel(1123353889228468305)
+          await channel.send(embed=embed)
+          await interaction.followup.send(f'Your application has been sent successfully', ephemeral=True)
 
 async def setup(bot: commands.Bot) -> None:
 	await bot.add_cog(Slash(bot))
