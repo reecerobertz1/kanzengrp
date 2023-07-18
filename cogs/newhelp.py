f4fb82ed7134371bfab995baf6a9ecae26d4408d
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-18 22:06:39 +0100

    Create newhelp.py

---
+++ cogs/newhelp.py
@@ -0,0 +1,56 @@
+from typing import Any
+import discord
+from discord.ext import commands
+from discord.interactions import Interaction
+from discord.ui import View, Select
+
+class HelpSelect(Select):
+    def __init__(self, bot: commands.bot):
+        super().__init__(
+            placeholder="Choose a category",
+            options=[
+                discord.SelectOption(
+            label=cog_name, description=cog.__doc__
+                ) for cog_name, cog in bot.cogs.items if cog.__cog__commands and cog_name not in ['Jishaku']
+            ]
+        )
+
+        self.bot = bot
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        cog = self.bot.get_cog(self.values[0])
+        assert cog
+
+        commands_mixer = []
+        for i in cog.walk_commands():
+            commands_mixer.append(i)
+
+        for i in cog.walk_commands():
+            commands_mixer.append(i)
+
+        embed = discord.Embed(
+            title=f'{cog.__cog_name__} Commands',
+            description='\n'.join(
+            f"**{command}**, `{command.description}`"
+            for command in commands_mixer
+            )
+        )
+        await interaction.response.send_message(embed=embed)
+
+
+class Utils(commands.cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+        @commands.hybrid_command(name=help, description='Shows list of commands')
+        async def help(self, ctx):
+            embed = discord.Embed(
+                title='Help command',
+                description='This is a help command'
+            )
+            view = View().add_item(HelpSelect(self.bot))
+            await ctx.send(embed=embed, view=view)
+
+async def setup(bot: commands.Bot) -> None:
+	await bot.add_cog(Utils(bot))
