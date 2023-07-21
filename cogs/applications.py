5145b86225177fdb2dcce292dff5603f64d920ec
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-21 17:03:43 +0100

    Update applications.py

--- cogs/applications.py
+++ cogs/applications.py
@@ -168,8 +168,8 @@ class applications(commands.Cog):
         if ctx.message.reference is not None:
             try:
                 msg = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-                user_id = msg.embeds[0].fields[0].value
-                embed = msg.embeds[0]
+                user_id = msg.embeds[6].fields[6].value
+                embed = msg.embeds[6]
                 embed.add_field(name="Status", value="Accepted ✅")
                 await ctx.message.add_reaction("✅")
                 await msg.edit(embed=embed)
