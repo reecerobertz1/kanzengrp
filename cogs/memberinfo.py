612ab484273456c4f5c1566214f757838673a021
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-26 17:29:09 +0100

    Update memberinfo.py

--- cogs/memberinfo.py
+++ cogs/memberinfo.py
@@ -56,7 +56,7 @@ class MemberInfo(commands.Cog):
             badgeslist.append("<a:938021210984419338:1122702983277330512> Booster")
 
     @commands.command()
-    async def userinfo(self, ctx, member: Optional[discord.Member] = None):
+    async def memberinfo(self, ctx, member: Optional[discord.Member] = None):
         if member is None:
             member = ctx.author
 
