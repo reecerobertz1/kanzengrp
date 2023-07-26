d9a60e8157ab0a75b5b1a911546b75635dbcd8e0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-26 18:05:47 +0100

    Update ttt.py

--- cogs/ttt.py
+++ cogs/ttt.py
@@ -4,6 +4,10 @@ import discord
 from discord.ext import commands
 import random
 
+class Games(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
 class TicTacToeButton(discord.ui.Button['TicTacToe']):
     def __init__(self, x, y):
         super().__init__(style=discord.ButtonStyle.secondary, label='\u200b')
@@ -36,57 +40,76 @@ class TicTacToeButton(discord.ui.Button['TicTacToe']):
 
             await view.message.edit(content=content, view=view)
 
-class TicTacToe(discord.ui.View):
-    def __init__(self, player1, player2):
-        super().__init__(timeout=30.0)
-        self.player_1 = player1
-        self.player_2 = player2
-        self.current_player = player1
-        self.board = [
-            [None, None, None],
-            [None, None, None],
-            [None, None, None]
-        ]
-        for x in range(3):
-            for y in range(3):
-                self.add_item(TicTacToeButton(x, y))
-
-    def check_board_winner(self):
-        for row in self.board:
-            if row[0] == row[1] == row[2] and row[0] is not None:
-                return row[0]
-
-        for col in range(3):
-            if self.board[0][col] == self.board[1][col] == self.board[2][col] and self.board[0][col] is not None:
-                return self.board[0][col]
-
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[0][0] is not None:
-            return self.board[0][0]
-
-        if self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[0][2] is not None:
-            return self.board[0][2]
-
-        if all(self.board[y][x] is not None for x in range(3) for y in range(3)):
-            return "tie"
-
-        return None
-
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        return interaction.user == self.current_player
-
-class Games(commands.Cog):
+class TicTacToe(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    @commands.command(aliases=['ttt'])
+    async def get_player_reaction(self, ctx, player, message):
+        try:
+            reaction, user = await self.bot.wait_for(
+                "reaction_add",
+                check=lambda reaction, user: user == player and reaction.message == message and str(reaction.emoji) in ["↖️", "⬆️", "↗️", "⬅️", "⏺️", "➡️", "↙️", "⬇️", "↘️"],
+                timeout=30.0,
+            )
+            return str(reaction.emoji)
+        except asyncio.TimeoutError:
+            return None
+
+    async def display_board(self, ctx, board):
+        message = "**Tic Tac Toe**\n"
+        for row in board:
+            message += " ".join([str(cell) if cell is not None else "\u200b" for cell in row])
+            message += "\n"
+        return await ctx.send(message)
+
     async def tictactoe(self, ctx: commands.Context, player2: discord.Member):
         if player2 == ctx.author:
             return await ctx.reply("You can't play yourself.")
 
-        view = TicTacToe(player1=ctx.author, player2=player2)
-        content = f'**Tic Tac Toe**\n<:x_:1087132262400794644> {ctx.author.mention} vs <:o_:1087132605876555807> {player2.mention}\n\nIt\'s {ctx.author.mention}\'s turn'
-        view.message = await ctx.send(content, view=view)
-        await view.wait()
+        player_1 = ctx.author
+        player_2 = player2
+        current_player = player_1
+        board = [[None, None, None], [None, None, None], [None, None, None]]
+
+        message = await self.display_board(ctx, board)
+
+        for x in range(3):
+            for y in range(3):
+                await TicTacToeButton(x, y).send(message)
+
+        while True:
+            reaction = await self.get_player_reaction(ctx, current_player, message)
+
+            if reaction is None:
+                return await ctx.send(f"{current_player.mention} didn't make a move in time. The game has ended.")
+
+            emoji_to_coords = {
+                "↖️": (0, 0),
+                "⬆️": (0, 1),
+                "↗️": (0, 2),
+                "⬅️": (1, 0),
+                "⏺️": (1, 1),
+                "➡️": (1, 2),
+                "↙️": (2, 0),
+                "⬇️": (2, 1),
+                "↘️": (2, 2),
+            }
+
+            x, y = emoji_to_coords[reaction]
+
+            if board[y][x] is not None:
+                await ctx.send("Invalid move! That cell is already taken.")
+                continue
+
+            if current_player == player_1:
+                board[y][x] = "X"
+                current_player = player_2
+            else:
+                board[y][x] = "O"
+                current_player = player_1
+
+            await message.edit(content=None)
+            await self.display_board(ctx, board)
         
 async def setup(bot):
     await bot.add_cog(Games(bot))
