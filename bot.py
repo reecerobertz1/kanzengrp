0fe3a5124883adb2fb61f82ecd0fcc9ae694373f
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-28 15:44:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,30 +64,30 @@ class LalisaBot(commands.Bot):
         self._BotBase__cogs = commands.core._CaseInsensitiveDict()
 
     # startup stuff
-    async def setup_hook(self):
-        print(f"Logged in as {self.user}")
+async def setup_hook(self):
+    print(f"Logged in as {self.user}")
 
-        # defining the attributes
-        self.launch_time = datetime.utcnow()
-        self.session = aiohttp.ClientSession()
-        self.pool = await asqlite.create_pool('databases/levels.db')
+    # defining the attributes
+    self.launch_time = datetime.utcnow()
+    self.session = aiohttp.ClientSession()
+    self.pool = await asqlite.create_pool('databases/levels.db')
 
-        # sqlite database setup (moved after the attribute assignments)
-        async with self.pool.acquire() as conn:
-            await conn.execute('''CREATE TABLE IF NOT EXISTS levels (member_id BIGINT, guild_id BIGINT, xp INTEGER, messages INTEGER, bar_color TEXT, PRIMARY KEY(member_id, guild_id))''')
-            await conn.execute('''CREATE TABLE IF NOT EXISTS setup (guild_id BIGINT PRIMARY KEY, activated BOOL, top_20_role_id BIGINT)''')
-            await conn.commit()
+    # sqlite database setup (moved after the attribute assignments)
+    async with self.pool.acquire() as conn:
+        await conn.execute('''CREATE TABLE IF NOT EXISTS levels (member_id BIGINT, guild_id BIGINT, xp INTEGER, messages INTEGER, bar_color TEXT, PRIMARY KEY(member_id, guild_id))''')
+        await conn.execute('''CREATE TABLE IF NOT EXISTS setup (guild_id BIGINT PRIMARY KEY, activated BOOL, top_20_role_id BIGINT)''')
+        await conn.commit()
 
-        # Check if the CommandTree instance already exists
-        if not hasattr(self, "tree"):
-            # Create a new CommandTree instance
-            self.tree = app_commands.CommandTree(self)
-            await self.tree.sync(guild=my_guild)
-            self.tree.copy_global_to(guild=my_guild)
+    # Check if the CommandTree instance already exists
+    if not hasattr(self, "tree"):
+        # Create a new CommandTree instance
+        self.tree = app_commands.CommandTree(self)
+        await self.tree.sync(guild=my_guild)
+        self.tree.copy_global_to(guild=my_guild)
 
-        # loads cogs/extensions
-        for ext in extensions:
-            await self.load_extension(ext)
+    # loads cogs/extensions
+    for ext in extensions:
+        await self.load_extension(ext)
 
     # i have to have this here for when the bot closes
     async def close(self):
