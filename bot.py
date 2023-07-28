8f64dc76f0dd1e080ddd32c1c5952bd05d67a8ea
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-28 15:00:09 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ class LalisaBot(commands.Bot):
     launch_time: datetime
     pool: asqlite.Pool
 
-    # initalising the subclass
+    # initializing the subclass
     # clarifying the intents
     # and then in super().__init__() set a prefix and add the intents
     def __init__(self):
@@ -54,6 +54,7 @@ class LalisaBot(commands.Bot):
             message_content=True,
             presences=True
         )
+        self.session = aiohttp.ClientSession()  # Initialize the session here
         super().__init__(
             command_prefix='+',
             intents=intents,
