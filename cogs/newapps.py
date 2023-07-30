4b2099621966f50b8a69c7cf53aba7c97d43732d
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-30 17:01:46 +0100

    Update newapps.py

--- cogs/newapps.py
+++ cogs/newapps.py
@@ -52,6 +52,11 @@ class Slash(commands.Cog):
     async def staffapp(self, interaction: discord.Interaction):
          await interaction.response.send_modal(staffapps())
 
+    @app_commands.command(name='staff', description='Apply for Kanzen staff!')
+    @app_commands.guilds(discord.Object(id=1121841073673736215))
+    async def staff(self, interaction: discord.Interaction):
+         await interaction.response.send_modal(staffapps())
+
 # modals
 
 class ia(ui.Modal, title='Inactivity Message'):
@@ -67,6 +72,28 @@ class ia(ui.Modal, title='Inactivity Message'):
           channel = interaction.client.get_channel(1121913672822968330)
           await channel.send(embed=embed)
           await interaction.followup.send(f'Your inactive message has been sent successfully', ephemeral=True)
+
+# kanzen staff
+
+class ia(ui.Modal, title='Kanzen Staff Apps'):
+     instagram = ui.TextInput(label='Instagram username', placeholder="Enter your Instagram username here...", style=discord.TextStyle.short)
+     reason = ui.TextInput(label='why should you be chosen?', placeholder="Put your reason here...", style=discord.TextStyle.long)
+     exp = ui.TextInput(label='what kind of expirience do you have?', placeholder="Expirience list here..", style=discord.TextStyle.long)
+     contribute = ui.TextInput(label='what will you contribute with?', placeholder="E.g. Events you would like to host, To help improve Kanzen etc...", style=discord.TextStyle.long)
+     extra = ui.TextInput(label='anything else you would like to add?', placeholder="Expirience list here..", style=discord.TextStyle.long)
+     async def on_submit(self, interaction: discord.Interaction):
+          await interaction.response.defer()
+          embed = discord.Embed(title='Inactivity Message', color=0x2b2d31)
+          embed.add_field(name='Instagram Name:', value=f'{self.instagram.value}', inline=False)
+          embed.add_field(name='Instagram Account Link:', value=f'https://instagram.com/{self.instagram.value}', inline=False)
+          embed.add_field(name='why should you be chosen?:', value=f'{self.reason.value}', inline=False)
+          embed.add_field(name='what kind of expirience do you have?:', value=f'{self.exp.value}', inline=False)
+          embed.add_field(name='what will you contribute with?:', value=f'{self.contribute.value}', inline=False)
+          embed.add_field(name='anything else you would like to say?', value=f'{self.extra.value}')
+          embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
+          channel = interaction.client.get_channel(1135238365902553158)
+          await channel.send(embed=embed)
+          await interaction.followup.send(f'You have sent your staff app successfully', ephemeral=True)
 
 # kanzen apps
 
