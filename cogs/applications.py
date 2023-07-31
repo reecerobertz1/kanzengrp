9693bd8b1544e527c3bc9a3d0f9b378be3f6ca65
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-07-31 13:25:37 +0100

    Update applications.py

--- cogs/applications.py
+++ cogs/applications.py
@@ -172,5 +172,41 @@ class applications(commands.Cog):
         else:
             await ctx.send("Please reply with the embed you want to process.")
 
+    @commands.command(aliases=['ds'])
+    @commands.has_permissions(manage_guild=True)
+    async def daegustaff(self, ctx):
+        if ctx.message.reference is not None:
+            try:
+                msg = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+                embed = msg.embeds[0]
+                user_id_field = next((field for field in embed.fields if field.name == 'Discord ID:'), None)
+
+                if user_id_field:
+                    user_id = user_id_field.value.strip()
+
+                    # DM the user with the decline message
+                    user = await ctx.guild.fetch_member(int(user_id))
+                    if user:
+                        decline_message = f"Hey! you have been accepted as a new staff member for Daegutowngrp!\nPlease go into <#1060656267670061106> for more information\nAlso join this server and ping in daegu's staff chat when you join!"
+                        await user.send(decline_message)
+
+                        # Edit the original embed to show the declined status
+                        embed = msg.embeds[0]
+                        embed.add_field(name="Status", value="Accepted ✅")
+                        await ctx.message.add_reaction("✅")
+                        await msg.edit(embed=embed)
+
+                        role_to_add = ctx.guild.get_role(903955975000698922)
+                        if role_to_add:
+                            await user.add_roles(role_to_add)
+                    else:
+                        await ctx.send("Failed to find the user.")
+                else:
+                    await ctx.send("Failed to find the Discord ID field in the embed.")
+            except Exception as e:
+                print(f"Failed to process the command: {e}")
+        else:
+            await ctx.send("Please reply with the embed you want to process.")
+
 async def setup(bot):
     await bot.add_cog(applications(bot))
