b235fca5822210502b64dc1c74c78f034ec78121
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-02 21:07:12 +0100

    Update starboard.py

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -4,39 +4,38 @@ from discord.ext import commands
 class Starboard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("Starboard cog is ready.")
+        self.error_channel_id = 1136384691671416932
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
-        # Check if the reaction is a star (⭐) emoji
-        if str(reaction.emoji) == "⭐" and reaction.message.guild.id == 1121841073673736215 and reaction.count >= min_stars:
-            # Set the minimum number of stars required for a message to be starred
-            min_stars = 3
+        try:
+            min_stars = 1  # Adjust this as needed
+            if str(reaction.emoji) == "⭐" and reaction.message.guild.id == 1121841073673736215 and reaction.count >= min_stars:
+                starboard_channel_id = 1136384691671416932
+                starboard_channel = self.bot.get_channel(starboard_channel_id)
 
-            # Check if the reaction count is greater than or equal to the minimum stars required
-            if reaction.count >= min_stars:
-                # Get the starboard channel by ID
-                starboard_channel = self.bot.get_channel(1136384691671416932)
+                if not starboard_channel:
+                    error_channel = self.bot.get_channel(self.error_channel_id)
+                    await error_channel.send(f"Starboard channel (ID: {starboard_channel_id}) not found.")
+                    return
 
-                # Create the embed
-                embed = discord.Embed(title="Starboard", description=reaction.message.content, color=0x2b2d31)
+                embed = discord.Embed(title="Starboard Message")
+
+                if reaction.message.content:
+                    embed.description = reaction.message.content
+
                 embed.set_author(name=reaction.message.author.display_name, icon_url=reaction.message.author.avatar_url)
+                embed.add_field(name="Original Message", value=f"[Jump!](https://discordapp.com/channels/{reaction.message.guild.id}/{reaction.message.channel.id}/{reaction.message.id})")
 
-                # Add a field for the original message link
-                jump_link = f"[jump!](https://discord.com/channels/{reaction.message.guild.id}/{reaction.message.channel.id}/{reaction.message.id})"
-                embed.add_field(name="Original Message", value=jump_link)
-
-                # Check if the message has an image
                 if reaction.message.attachments:
-                    # Use the first attachment as the image URL
                     image_url = reaction.message.attachments[0].url
                     embed.set_image(url=image_url)
 
-                # Send the embed to the starboard channel
                 await starboard_channel.send(embed=embed)
+
+        except Exception as e:
+            error_channel = self.bot.get_channel(self.error_channel_id)
+            await error_channel.send(f"An error occurred: {e}")
 
 async def setup(bot):
     await bot.add_cog(Starboard(bot))
