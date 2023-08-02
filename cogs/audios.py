233911fdbcad94af13da36d7e051c01cadb90092
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-02 19:55:14 +0100

    new audio

--- cogs/audios.py
+++ cogs/audios.py
@@ -7,28 +7,46 @@ class audios(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def addaudio(self, ctx, link):
+    async def _add_audio(self, ctx, filename, link):
         audio_data = link
         try:
-            with open("addedaudios.json", "r") as file:
+            with open(filename, "r") as file:
                 data = json.load(file)
         except FileNotFoundError:
             data = []
 
         data.append(audio_data)
 
-        with open("addedaudios.json", "w") as file:
+        with open(filename, "w") as file:
             json.dump(data, file, indent=4)
 
         await ctx.reply("Audio added successfully.")
 
-    @commands.group(aliases=['audios'])
-    async def audio(self, ctx):
-        with open("addedaudios.json", "r") as f:
+    @commands.command()
+    async def addsoft(self, ctx, link):
+        """Adds a streamable link to softaudios.json"""
+        await self._add_audio(ctx, "softaudios.json", link)
+
+    @commands.command()
+    async def addhot(self, ctx, link):
+        """Adds a streamable link to hotaudios.json"""
+        await self._add_audio(ctx, "hotaudios.json", link)
+
+    @commands.command(aliases=['softaudio'])
+    async def soft(self, ctx):
+        """Sends a random link from softaudios.json"""
+        with open("softaudios.json", "r") as f:
             audios = json.load(f)
             choice = random.choice(audios)
-            await ctx.reply(f"Add an audio with `+addaudio`\n{choice}")
+            await ctx.reply(f"Add a soft audio with `+addsoft`\n{choice}")
+
+    @commands.command(aliases=['hotaudio'])
+    async def hot(self, ctx):
+        """Sends a random link from hotaudios.json"""
+        with open("hotaudios.json", "r") as f:
+            audios = json.load(f)
+            choice = random.choice(audios)
+            await ctx.reply(f"Add a hot audio with `+addhot`\n{choice}")
 
 async def setup(bot):
     await bot.add_cog(audios(bot))
