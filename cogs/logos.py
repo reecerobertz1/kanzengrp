2583aa3db394eea3eeac05175a32d94deab57f08
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 17:47:50 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -150,15 +150,15 @@ class logos(commands.Cog):
         embed = discord.Embed(title="Welcome to Editors Block!", description="Thank you for joining Editors Block! This is a community server made for all types of editors.\nFeel free to ping @owners or @staff if you need any help.\n\nAlso, we will do group recruits for the groups Kanzen, Aura, and Daegu!", color=0x2b2d31)
         embed.set_footer(text="Follow the groups below!", icon_url='https://cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png?size=1024')
 
-        button = discord.ui.Button(label="Kanzen", url="https://www.instagram.com/kanzengrp/")
-        button2 = discord.ui.Button(label="Aura", url="https://www.instagram.com/auragrps/")
-        button3 = discord.ui.Button(label="Daegu", url="https://www.instagram.com/daegutowngrp/")
+        button = discord.ui.Button(label="Kanzen", url="https://www.instagram.com/kanzengrp/", emoji="<:kanzen:1136701626799886366>")
+        button2 = discord.ui.Button(label="Aura", url="https://www.instagram.com/auragrps/", emoji="<:aura:1136701593018978415>")
+        button3 = discord.ui.Button(label="Daegu", url="https://www.instagram.com/daegutowngrp/", url="<:daegu:1136701608026185879>")
 
         view = discord.ui.View()
         view.add_item(button)
         view.add_item(button2)
         view.add_item(button3)
-        
+
         await ctx.send(embed=embed, view=view)
 
         embed2 = discord.Embed(title="Owner Info", description="Editors Block is owned by @remqsi, @yoongiaeps, and @taedxck")
