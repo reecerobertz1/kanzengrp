9f2ee833e9fefa458c19849eacc07596cca74f42
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 17:08:59 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -150,9 +150,9 @@ class logos(commands.Cog):
         embed = discord.Embed(title="Welcome !", description="> Thank you for joining Editors Block! This is a community server made for all types of editors.\nFeel free to ping @owners or @staff if you need any help.\n\nAlso we will do group recruits for the groups Kanzen, Aura and Daegu!", color=0x2b2d31)
         embed.set_footer(text="Follow the groups below!", icon_url=' https://cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png?size=1024 ')
 
-        button = discord.ui.Button(label="Kanzen", url=" https://www.instagram.com/kanzengrp/ ")
-        button2 = discord.ui.Button(label="Aura", url=" https://www.instagram.com/auragrps/ ")
-        button3 = discord.ui.Button(label="Daegu", url=" https://www.instagram.com/daegutowngrp/ ")
+        button = discord.ui.Button(label="Kanzen", url="https://www.instagram.com/kanzengrp/")
+        button2 = discord.ui.Button(label="Aura", url="https://www.instagram.com/auragrps/")
+        button3 = discord.ui.Button(label="Daegu", url="https://www.instagram.com/daegutowngrp/")
 
         view = discord.ui.View()
         view.add_item(button)
