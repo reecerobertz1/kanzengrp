2ca148ffe1854c6f72c71ed6787cf18c52f7225a
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 17:38:24 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -147,11 +147,14 @@ class logos(commands.Cog):
 
     @commands.command()
     async def lol(self, ctx):
-        embed = discord.Embed(title="Welcome !", description="> Thank you for joining Editors Block! This is a community server made for all types of editors.\n> Feel free to ping @owners or @staff if you need any help.\n\nAlso we will do group recruits for the groups Kanzen, Aura and Daegu!", color=0x2b2d31)
-        embed.set_footer(text="Follow the groups below!", icon_url=' https://cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png?size=1024 ')
+        embed = discord.Embed(title="Welcome to Editors Block!", description="Thank you for joining Editors Block! This is a community server made for all types of editors.\nFeel free to ping @owners or @staff if you need any help.\n\nAlso, we will do group recruits for the groups Kanzen, Aura, and Daegu!", color=0x2b2d31)
+        embed.set_footer(text="Follow the groups below!", icon_url='https://cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png?size=1024')
+        await ctx.send(embed=embed)
 
-        embed2 = discord.Embed(title="Owner Info", description="Editors Block is owned by @remqsi, @yoongiaeps and @taedxck")
-        embed2.set_author(name="Hoshi#3105", icon_url=f'https://cdn.discordapp.com/avatars/849682093575372841/f04c5815341216fdafe736a2564a4d09.png?size=1024')
+        embed2 = discord.Embed(title="Owner Info", description="Editors Block is owned by @remqsi, @yoongiaeps, and @taedxck")
+        embed2.set_author(name="Hoshi#3105", icon_url='https://cdn.discordapp.com/avatars/849682093575372841/f04c5815341216fdafe736a2564a4d09.png?size=1024')
+        await ctx.send(embed=embed2)
+
         button = discord.ui.Button(label="Kanzen", url="https://www.instagram.com/kanzengrp/")
         button2 = discord.ui.Button(label="Aura", url="https://www.instagram.com/auragrps/")
         button3 = discord.ui.Button(label="Daegu", url="https://www.instagram.com/daegutowngrp/")
@@ -161,7 +164,7 @@ class logos(commands.Cog):
         view.add_item(button2)
         view.add_item(button3)
 
-        await ctx.send(embed=embed, embed=embed2, view=view)
+        await ctx.send("Follow the groups below!", view=view)
         
 
 async def setup(bot):
