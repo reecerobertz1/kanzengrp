6ad50998835f0550743c6d591361a7cce449c39c
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 17:16:10 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -150,6 +150,7 @@ class logos(commands.Cog):
         embed = discord.Embed(title="Welcome !", description="> Thank you for joining Editors Block! This is a community server made for all types of editors.\n> Feel free to ping @owners or @staff if you need any help.\n\nAlso we will do group recruits for the groups Kanzen, Aura and Daegu!", color=0x2b2d31)
         embed.set_footer(text="Follow the groups below!", icon_url=' https://cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png?size=1024 ')
 
+        embed2 = discord.Embed(title="Owner Info", description="Editors Block is owned by @remqsi, @yoongiaeps and @taedxck")
         button = discord.ui.Button(label="Kanzen", url="https://www.instagram.com/kanzengrp/")
         button2 = discord.ui.Button(label="Aura", url="https://www.instagram.com/auragrps/")
         button3 = discord.ui.Button(label="Daegu", url="https://www.instagram.com/daegutowngrp/")
@@ -159,7 +160,8 @@ class logos(commands.Cog):
         view.add_item(button2)
         view.add_item(button3)
 
-        await ctx.send(embed=embed, view=view)
+        await ctx.send(embed=embed, view=view, embed2=embed2)
+        
 
 async def setup(bot):
     await bot.add_cog(logos(bot))
