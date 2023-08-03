9b3f7e6b5663d407dfd4bc7b01a1422180520eb8
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 17:06:16 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -170,6 +170,21 @@ class logos(commands.Cog):
             channel = server.get_channel(server_data['channel_id'])
             await channel.send(server_data['ping_role'], embed=embed)
 
+    @commands.command()
+    async def lol(self, ctx):
+        embed = discord.Embed(title="Welcome !", description="> Thank you for joining Editors Block! This is a community server made for all types of editors.\nFeel free to ping @owners or @staff if you need any help.\n\nAlso we will do group recruits for the groups Kanzen, Aura and Daegu!", color=0x2b2d31)
+        embed.set_footer(text="Follow the groups below!", icon_url=' https://cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png?size=1024 ')
+
+        button = discord.ui.Button(label="Kanzen", url=" https://www.instagram.com/kanzengrp/ ")
+        button2 = discord.ui.Button(label="Aura", url=" https://www.instagram.com/auragrps/ ")
+        button3 = discord.ui.Button(label="Daegu", url=" https://www.instagram.com/daegutowngrp/ ")
+
+        view = discord.ui.View()
+        view.add_item(button)
+        view.add_item(button2)
+        view.add_item(button3)
+
+        await ctx.send(embed=embed, view=view)
 
 async def setup(bot):
     await bot.add_cog(logos(bot))
