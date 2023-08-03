8daf29e138fc68bd7dde06b3a94ae985f2edbb3f
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 17:57:26 +0100

    Update logos.py

--- cogs/logos.py
+++ cogs/logos.py
@@ -164,8 +164,8 @@ class logos(commands.Cog):
         embed2 = discord.Embed(title="Owner Info", description="Editors Block is owned by @remqsi, @yoongiaeps, and @taedxck", color=0x2b2d31)
         embed2.set_author(name="Hoshi#3105", icon_url='https://cdn.discordapp.com/avatars/849682093575372841/f04c5815341216fdafe736a2564a4d09.png?size=1024')
 
-        button_rules = discord.ui.Button(label="Server Rules", style=discord.ButtonStyle.primary)
-        button_roles = discord.ui.Button(label="Role Info", style=discord.ButtonStyle.primary)
+        button_rules = discord.ui.Button(label="Server Rules", style=discord.ButtonStyle.primary, custom_id="server_rules")
+        button_roles = discord.ui.Button(label="Role Info", style=discord.ButtonStyle.primary, custom_id="role_info")
 
         view2 = discord.ui.View()
         view2.add_item(button_rules)
@@ -175,12 +175,12 @@ class logos(commands.Cog):
 
     @commands.Cog.listener()
     async def on_button_click(self, interaction):
-        if interaction.component.label == "Server Rules":
+        if interaction.custom_id == "server_rules":
             embed_rules = discord.Embed(title="Server Rules", description="Here are the server rules:\n1. No spamming.\n2. Be respectful to others.\n3. No NSFW content.\n4. No advertising.\n5. Follow Discord's Terms of Service and Community Guidelines.")
-            await interaction.channel.send(embed=embed_rules, ephemeral=False)
-        elif interaction.component.label == "Role Info":
+            await interaction.response.send_message(embed=embed_rules)
+        elif interaction.custom_id == "role_info":
             embed_roles = discord.Embed(title="Role Info", description="Here's some information about the roles in the server:\n- Owner: The owners of the server.\n- Staff: The staff members who help moderate the server.\n- Members: Regular members of the server.")
-            await interaction.channel.send(embed=embed_roles, ephemeral=False)
+            await interaction.response.send_message(embed=embed_roles)
         
 
 async def setup(bot):
