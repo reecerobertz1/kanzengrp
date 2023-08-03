4a16f69a804dfa6cd43e14585f70b14ffd0ef09c
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-03 21:56:50 +0100

    Update eb.py

--- cogs/eb.py
+++ cogs/eb.py
@@ -12,7 +12,7 @@ class infobuttons(discord.ui.View):
         self.value = None 
 
     @discord.ui.button(label="Server Guide", emoji="<:guide:1136757467943022685>")
-    async def roles(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def guide(self, interaction: discord.Interaction, button: discord.ui.Button):
         embed = discord.Embed(title="<:guide:1136757467943022685> Server Guide", color=0x2b2d31)
         embed.add_field(name="Get Started", value="<#1131007362895970414> - Server announcements are made here!\n<#1131005271502753812> - Server information\n<#1133730290221715487> - Get your roles here", inline=False)
         embed.add_field(name="Boost", value="<#1133772140915732511> - Boost messages are sent here\n<#1133772155499327538> - Claim the booster perks here", inline=False)
@@ -70,8 +70,11 @@ class ebmessages(commands.Cog):
 
     @commands.command()
     async def lol(self, ctx):
-        embed = discord.Embed(title="Welcome *!*", description="> Thank you for joining Editors Block!\n> This is a community server made for all types of editors.\n> Feel free to ping @owners or @staff if you need any help.\n\nAlso, we will do group recruits for the groups Kanzen, Aura, and Daegu!", color=0x2b2d31)
-        embed.set_footer(text="Follow the groups below!", icon_url="https://images-ext-2.discordapp.net/external/eVpj5e3hkU4cFDmIU8KnoGkfnfyDbbJPVs1xJWmUNQg/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png")
+        embed = discord.Embed(title="Welcome *!*", 
+                              description="> Thank you for joining Editors Block!\n> This is a community server made for all types of editors.\n> Feel free to ping @owners or @staff if you need any help.\n\nAlso, we will do group recruits for the groups Kanzen, Aura, and Daegu!", 
+                              color=0x2b2d31)
+        embed.set_footer(text="Follow the groups below!", 
+                         icon_url="https://images-ext-2.discordapp.net/external/eVpj5e3hkU4cFDmIU8KnoGkfnfyDbbJPVs1xJWmUNQg/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1131003330810871979/128ca9e19d2f0aa0e41c99310673dfac.png")
 
         button = discord.ui.Button(label="Kanzen", url="https://www.instagram.com/kanzengrp/", emoji="<:kanzen:1136701626799886366>")
         button2 = discord.ui.Button(label="Aura", url="https://www.instagram.com/auragrps/", emoji="<:aura:1136701593018978415>")
@@ -86,7 +89,9 @@ class ebmessages(commands.Cog):
     @commands.command()
     async def lol2(self, ctx):
         view = infobuttons()
-        embed = discord.Embed(title="<:rules:1136761913972359178> Server Rules", description="<a:arrowpink:1134860720777990224> Follow Discord's [tos](https://discord.com/terms) and [guidelines](https://discord.com/guidelines)\n<a:arrowpink:1134860720777990224> Be nice and respectful to everyone in the server!\n<a:arrowpink:1134860720777990224> No impersonation of other editors (you will be banned)\n<a:arrowpink:1134860720777990224> Use channels for their intended purpose\n<a:arrowpink:1134860720777990224> No spamming pings, you will be warned and then kicked\n<a:arrowpink:1134860720777990224>  No trash talk of other people", color=0xee518f)
+        embed = discord.Embed(title="<:rules:1136761913972359178> Server Rules", 
+                                description="<a:arrowpink:1134860720777990224> Follow Discord's [tos](https://discord.com/terms) and [guidelines](https://discord.com/guidelines)\n<a:arrowpink:1134860720777990224> Be nice and respectful to everyone in the server!\n<a:arrowpink:1134860720777990224> No impersonation of other editors (you will be banned)\n<a:arrowpink:1134860720777990224> Use channels for their intended purpose\n<a:arrowpink:1134860720777990224> No spamming pings, you will be warned and then kicked\n<a:arrowpink:1134860720777990224>  No trash talk of other people", 
+                                color=0xee518f)
         await ctx.send(embed=embed, view=view)
 
 async def setup(bot):
