18c4e49460e49685a93398a720c30d56a28203c3
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-05 16:17:13 +0100

    Update eb.py

--- cogs/eb.py
+++ cogs/eb.py
@@ -63,12 +63,9 @@ class answer(discord.ui.View):
 
 class verifymodal(ui.Modal, title='Verification'):
      code = ui.TextInput(label='What was the code?', placeholder="Enter code here...", style=discord.TextStyle.short)
-     async def on_submit(self, interaction: discord.Interaction):
-        if (role) is not discord.Role:
-            role = interaction.guild.get_role(1131016147282710679)
-        if role not in interaction.user.roles:
-            await interaction.user.add_roles(role)
-        await interaction.response.send_message("Thank you! You're all verified, enjoy your time here in Editors Block", ephemeral=True)
+     async def on_submit(self, interaction: discord.Interaction, role: 1131016147282710679):
+        await interaction.user.add_roles(role)
+        await interaction.response.send_message(f"Thank you {interaction.user.name}! You're all verified, enjoy your time here in Editors Block", ephemeral=True)
 
 class qnabutton(discord.ui.View):
     def __init__ (self):
