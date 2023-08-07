bfabbfbac42e5ffcff288ba6a4b55bf4d9f8b404
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-07 20:56:09 +0100

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -10,48 +10,27 @@ class Roles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-class Roles(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.command()
     async def roles(self, ctx):
+        await ctx.send("Please select your pronouns:", view=self.get_pronouns_menu())
+
+    def get_pronouns_menu(self):
         pronouns_menu = discord.ui.Select(placeholder="Please select your pronouns:", min_values=1, max_values=1)
-        pronouns_menu.add_option(label="She/Her", value="she_her")
-        pronouns_menu.add_option(label="He/Him", value="he_him")
-        pronouns_menu.add_option(label="They/Them", value="they_them")
-
-        server_pings_menu = discord.ui.Select(placeholder="Please select your server pings:", min_values=1, max_values=1)
-        server_pings_menu.add_option(label="Announcements", value="announcements")
-        server_pings_menu.add_option(label="Events", value="events")
-        server_pings_menu.add_option(label="Giveaways", value="giveaways")
-
-        member_pings_menu = discord.ui.Select(placeholder="Please select your member pings:", min_values=1, max_values=1)
-        member_pings_menu.add_option(label="Group Updates", value="group_updates")
-        member_pings_menu.add_option(label="Group Auditions", value="group_auditions")
-        member_pings_menu.add_option(label="Collab Requests", value="collab_requests")
-
-        view = discord.ui.View()
-        view.add_item(pronouns_menu)
-        view.add_item(server_pings_menu)
-        view.add_item(member_pings_menu)
-
-        await ctx.send("Please select your roles:", view=view)
+        pronouns_menu.add_option(label="She/Her", value="she_her", role_id=1122635691487137884)
+        pronouns_menu.add_option(label="He/Him", value="he_him", role_id=1121852424353755137)
+        pronouns_menu.add_option(label="They/Them", value="they_them", role_id=1122635724559241317)
+        return pronouns_menu
 
     @commands.Cog.listener()
     async def on_select_option(self, interaction: discord.Interaction):
+        role_id = None
         if interaction.custom_id == "pronouns_menu":
-            pronouns = interaction.values[0]
-            role_id = 1122635691487137884  # Replace with the role ID for the selected pronoun
+            role_id = interaction.data["values"][0].get("role_id")
+
+        if role_id:
             role = discord.utils.get(interaction.guild.roles, id=role_id)
             if role:
                 await interaction.user.add_roles(role)
-        elif interaction.custom_id == "server_pings_menu":
-            # Handle server pings role assignment here
-            pass
-        elif interaction.custom_id == "member_pings_menu":
-            # Handle member pings role assignment here
-            pass
 
 async def setup(bot):
     await bot.add_cog(Roles(bot))
