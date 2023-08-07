c3582f39c29c9f96cd0b6de545553a955079f1d8
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-07 21:02:44 +0100

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -16,16 +16,21 @@ class Roles(commands.Cog):
 
     def get_pronouns_menu(self):
         pronouns_menu = discord.ui.Select(placeholder="Please select your pronouns:", min_values=1, max_values=1)
-        pronouns_menu.add_option(label="She/Her", value="she_her", role_id=1122635691487137884)
-        pronouns_menu.add_option(label="He/Him", value="he_him", role_id=1121852424353755137)
-        pronouns_menu.add_option(label="They/Them", value="they_them", role_id=1122635724559241317)
+        pronouns_options = {
+            "She/Her": 1122635691487137884,
+            "He/Him": 1121852424353755137,
+            "They/Them": 1122635724559241317
+        }
+        for label, role_id in pronouns_options.items():
+            pronouns_menu.add_option(label=label, value=label, description="Select this option.", emoji="👍", role_id=role_id)
         return pronouns_menu
 
     @commands.Cog.listener()
     async def on_select_option(self, interaction: discord.Interaction):
         role_id = None
         if interaction.custom_id == "pronouns_menu":
-            role_id = interaction.data["values"][0].get("role_id")
+            selected_option = interaction.data["values"][0]
+            role_id = selected_option.get("role_id")
 
         if role_id:
             role = discord.utils.get(interaction.guild.roles, id=role_id)
