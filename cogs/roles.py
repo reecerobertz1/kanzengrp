7853fa91686baad3e96a36060906071b039384a3
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-07 22:38:03 +0100

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -27,37 +27,37 @@ class Roles(commands.Cog):
         )
         async def add_role(interaction: discord.Interaction):
             await interaction.response.defer()
-            if select.values[0] == "he/him":
-                member = interaction.user
-                role_id = 1131127428668997737
+            
+            # Define the role IDs and their corresponding labels
+            role_ids = {
+                "he/him": 1131127428668997737,
+                "she/her": 1131127209952809031,
+                "they/them": 1131127449753751663,
+                "he/they": 1131127472142958622,
+                "she/they": 1131127502396465213,
+                "any/ask": 1131127523456069723
+            }
+            
+            member = interaction.user
+            selected_value = interaction.data["values"][0]
+            selected_role_id = role_ids.get(selected_value)
+            
+            # Remove existing pronoun roles from the user
+            for role_id in role_ids.values():
                 role = interaction.guild.get_role(role_id)
-                await member.add_roles(role)
-            elif select.values[0] == "she/her":
-                member = interaction.user
-                role_id = 1131127209952809031
-                role = interaction.guild.get_role(role_id)
-                await member.add_roles(role)
-            elif select.values[0] == "they/them":
-                role_id = 1131127449753751663
-                role = interaction.guild.get_role(role_id)
-                member = interaction.user
-                await member.add_roles(role)
-            elif select.values[0] == "he/they":
-                role_id = 1131127472142958622
-                role = interaction.guild.get_role(role_id)
-                member = interaction.user
-                await member.add_roles(role)
-            elif select.values[0] == "she/they":
-                role_id = 1131127502396465213
-                role = interaction.guild.get_role(role_id)
-                member = interaction.user
-                await member.add_roles(role)
-            elif select.values[0] == "any/ask":
-                role_id = 1131127523456069723
-                role = interaction.guild.get_role(role_id)
-                member = interaction.user
-                await member.add_roles(role)
-            await interaction.followup.send(f"You selected {select.values[0]}", ephemeral=True)
+                if role and role in member.roles:
+                    await member.remove_roles(role)
+            
+            if selected_role_id:
+                role = interaction.guild.get_role(selected_role_id)
+                if role:
+                    await member.add_roles(role)
+                    await interaction.followup.send(f"You selected {selected_value}", ephemeral=True)
+                else:
+                    await interaction.followup.send("Role not found. Please contact a server admin.", ephemeral=True)
+            else:
+                await interaction.followup.send("Invalid role selection. Please try again.", ephemeral=True)
+
         select.callback = add_role
         view = View()
         view.add_item(select)
@@ -65,6 +65,7 @@ class Roles(commands.Cog):
         embed = discord.Embed(title="<:leaf:1137454366886993950> What are your pronouns?", description="Which of the following pronouns do you use? These roles can help other members of the server use the correct pronouns.\n\n<:1:1137455321028251708> - <@&1131127428668997737> \n<:2:1137455517577531565> - <@&1131127209952809031> \n<:3:1137455658258673704> - <@&1131127449753751663> \n<:4:1137455776877781107> - <@&1131127472142958622> \n<:5:1137455941609078824> - <@&1131127502396465213> \n<:6:1137456046978383892> - <@&1131127523456069723>", color=0x2b2d31)
         await ctx.send("https://cdn.discordapp.com/attachments/1131006428631539773/1138222545187909713/pronouns_new_00000_2.png")
         await ctx.send(embed=embed, view=view)
+        await ctx.send("<a:space:862938652694151169>")
 
 async def setup(bot):
     await bot.add_cog(Roles(bot))
