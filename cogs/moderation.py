c7f36bf20fee73391e684f7462af3bcc3c225015
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-08 01:59:00 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -172,12 +172,10 @@ class Moderation(commands.Cog):
 
     @commands.command()
     async def rules(self, ctx):
-
-        async def ia(interaction: discord.Interaction):
-            await interaction.response.send_modal(ia())
-
         view = inactive()
-        embed = discord.Embed(title="Kanzen rules", description="✦ Group Rules :\n<a:bounceyarrow:1128155233437106187> always watermark the logos\n<a:bounceyarrow:1128155233437106187> do not share the logos link outside the server!\n<a:bounceyarrow:1128155233437106187> make sure you are following @remqsi + @kanzengrp!\n<a:bounceyarrow:1128155233437106187> if you do ever decide to leave the grp, or move accounts. please let lead or staff know!\n✦ Chat Rules :\n<a:bounceyarrow:1128155233437106187> please be as active as possible!\n<a:bounceyarrow:1128155233437106187> no using any slurs / words that can be offensive!\n<a:bounceyarrow:1128155233437106187> please set your nickname as 'your name | username'\n<a:bounceyarrow:1128155233437106187> no impersonation as other editors\n<a:bounceyarrow:1128155233437106187> no trash talking other editors and groups!", color=0x2b2d31)
+        embed = discord.Embed(title="Kanzen rules", description="✦ Group Rules :\n<a:bounceyarrow:1128155233437106187> always watermark the logos\n<a:bounceyarrow:1128155233437106187> do not share the logos link outside the server!\n<a:bounceyarrow:1128155233437106187> make sure you are following @remqsi + @kanzengrp!\n<a:bounceyarrow:1128155233437106187> if you do ever decide to leave the grp, or move accounts. please let lead or staff know!\n\n✦ Chat Rules :\n<a:bounceyarrow:1128155233437106187> please be as active as possible!\n<a:bounceyarrow:1128155233437106187> no using any slurs / words that can be offensive!\n<a:bounceyarrow:1128155233437106187> please set your nickname as 'your name | username'\n<a:bounceyarrow:1128155233437106187> no impersonation as other editors\n<a:bounceyarrow:1128155233437106187> no trash talking other editors and groups!", color=0x2b2d31)
+        embed.set_image(url=f"https://cdn.discordapp.com/attachments/1121841074512605186/1128422069336543232/Comp_1_00000.png")
+        embed.set_footer(text="If you need to go inactive, use the button below!", icon_url=ctx.guild.icon)
         await ctx.send("https://cdn.discordapp.com/attachments/1121841074512605186/1138272212948881498/kanzen_rules_00000.png")
         await ctx.send(embed=embed, view=view)
 
