c934b74962d6acc139e4fc9a3167d5e59fe86f80
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-08 01:04:40 +0100

    Update eb.py

--- cogs/eb.py
+++ cogs/eb.py
@@ -172,10 +172,9 @@ class grprctkda(ui.Modal, title='Applications'):
           embed.add_field(name='Anything else:', value=f'{self.extra.value}', inline=False)
           embed.add_field(name="Discord ID:", value=interaction.user.id, inline=False)
           channel = interaction.client.get_channel(1131006328207327294)
-          response_message = await interaction.followup.send(f'Your application has been sent successfully', ephemeral=True)
-          await channel.send(embed=embed)
+          form = await channel.send(embed=embed)
           await interaction.followup.send(f'Your application has been sent successfully', ephemeral=True)
-          await response_message.add_reaction("✅")
+          await form.add_reaction("✅")
 
 class ebmessages(commands.Cog):
     def __init__(self, bot):
