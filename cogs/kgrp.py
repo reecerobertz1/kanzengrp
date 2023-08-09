27515bc17068708d3d2ae9aef1eca91a6acb3aff
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-09 05:18:03 +0100

    Update kgrp.py

--- cogs/kgrp.py
+++ cogs/kgrp.py
@@ -24,16 +24,15 @@ class infobuttons(discord.ui.View):
         logos.set_image(url="https://cdn.discordapp.com/attachments/1121841074512605186/1128394231115948072/theme_3_00000.png")
         await interaction.user.send(embed=logos, view=view)
 
-        channel = interaction.client.get_channel(1122627075682078720)
-        log = discord.Embed(title="Logo button has been used!", description=f"`{interaction.user.display_name}` has used the logos button")
-        log.set_footer(text=f"id: {interaction.user.id}", icon_url=interaction.user.display_avatar)
-        await channel.send(embed=log)
-        
         try:
-            await asyncio.sleep(2)  # Add a delay of 2 seconds
-            await interaction.followup.send(f'I have sent you the logos! Check your DMs', ephemeral=True)
+            followup = await interaction.followup.send(f'I have sent you the logos! Check your DMs', ephemeral=True)
+            channel = interaction.client.get_channel(1122627075682078720)
+            log = discord.Embed(title="Logo button has been used!", description=f"`{interaction.user.display_name}` has used the logos button")
+            log.set_footer(text=f"id: {interaction.user.id}", icon_url=interaction.user.display_avatar)
+            await channel.send(embed=log)
         except Exception as e:
             print(f"An error occurred while sending the follow-up message: {e}")
+
 
     @discord.ui.button(label="Inactive", emoji="<a:kanzenflower:1128154723262943282>")
     async def inactive(self, interaction: discord.Interaction, button: discord.Button):
