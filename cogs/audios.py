56dcd0bf3f078c0f8cbfb9d7dfdff7c6d476a1b0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-09 16:21:29 +0100

    Update audios.py

--- cogs/audios.py
+++ cogs/audios.py
@@ -35,7 +35,7 @@ class audios(commands.Cog):
         """Adds a streamable link to softaudios.json"""
         log = 1122627075682078720
         embed = discord.Embed(title="Added soft audio", description=f"`{ctx.author.display_name} added a soft audio\n{link}")
-        embed.footer(text=f"id: {ctx.author.id}", icon_url=ctx.author.display_icon)
+        embed.set_footer(text=f"id: {ctx.author.id}", icon_url=ctx.author.avatar)
         await log.send(embed=embed)
         await self._add_audio(ctx, "softaudios.json", link)
 
@@ -44,7 +44,7 @@ class audios(commands.Cog):
         """Adds a streamable link to hotaudios.json"""
         log = 1122627075682078720
         embed = discord.Embed(title="Added hot audio", description=f"`{ctx.author.display_name} added a hot audio\n{link}")
-        embed.footer(text=f"id: {ctx.author.id}", icon_url=ctx.author.display_icon)
+        embed.set_footer(text=f"id: {ctx.author.id}", icon_url=ctx.author.avatar)
         await log.send(embed=embed)
         await self._add_audio(ctx, "hotaudios.json", link)
 
