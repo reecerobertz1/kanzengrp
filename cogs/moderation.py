61603b196b2cb80855abc22bb417c0cd4f372897
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-17 02:04:09 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -224,13 +224,13 @@ class Moderation(commands.Cog):
             await message.edit(content="~~Are you sure you want to send this update message?~~\nThe update has been cancelled")
 
     @commands.command()
-    async def newwelc(self, ctx):
+    async def newwelc(self, ctx, member: discord.Member):
         embed = discord.Embed(color=0x2b2d31, description=f"<a:arrowlightpink:1141452054716489789> [read infortmation](https://discord.com/channels/1131003330810871979/1131005271502753812)\n<a:arrowlightpink:1141452054716489789> [get roles](https://discord.com/channels/1131003330810871979/1133730290221715487)\n<a:arrowlightpink:1141452054716489789> [apply here](https://discord.com/channels/1131003330810871979/1133771634793250847)")
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
         timestamp = datetime.datetime.utcnow()
         embed.timestamp = timestamp
-        embed.set_footer(text=ctx.guild.member_count)
+        embed.set_footer(text=member.guild.member_count)
         await ctx.send(f'<:bubbles:1141532181206929438> Welcome {ctx.author.mention}! <@&1131005057417105418>', embed=embed)
 
 async def setup(bot):
