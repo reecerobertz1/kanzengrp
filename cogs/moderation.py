85236b2d4f67cc69e5945d28e9123140293b57fc
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-17 02:20:13 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -226,13 +226,14 @@ class Moderation(commands.Cog):
 
     @commands.command()
     async def newwelc(self, ctx):
-        editorsblock = ctx.guild.id(1121841073673736215)
+        members = [member for member in ctx.guild.members if not member.bot]
+        member_count = len(members)
         embed = discord.Embed(color=0x2b2d31, description=f"<a:arrowlightpink:1141452054716489789> [read infortmation](https://discord.com/channels/1131003330810871979/1131005271502753812)\n<a:arrowlightpink:1141452054716489789> [get roles](https://discord.com/channels/1131003330810871979/1133730290221715487)\n<a:arrowlightpink:1141452054716489789> [apply here](https://discord.com/channels/1131003330810871979/1133771634793250847)")
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
         timestamp = datetime.datetime.utcnow()
         embed.timestamp = timestamp
-        embed.set_footer(text=f"{sum(g.member_count for g in editorsblock)}")
+        embed.set_footer(text=f"{member_count}")
         await ctx.send(f'<:bubbles:1141532181206929438> Welcome {ctx.author.mention}! <@&1131005057417105418>', embed=embed)
 
 async def setup(bot):
