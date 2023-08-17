a6c5d68d8b70f3c5d69083dce1d52c662a25b896
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-08-17 23:42:03 +0100

    Update starboard.py

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -10,7 +10,7 @@ class Starboard(commands.Cog):
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, member):
         try:
-            min_stars = 3
+            min_stars = 1
             if (
                 str(reaction.emoji) == "⭐"
                 and reaction.message.guild.id == 1121841073673736215
