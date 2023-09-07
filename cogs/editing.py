be451023c82e27093edcd64f7c3214492e71f71b
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-09-07 19:52:45 +0100

    Update editing.py

--- cogs/editing.py
+++ cogs/editing.py
@@ -6,9 +6,22 @@ class editing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["effect"])
-    async def effects(self, ctx):
+    @commands.group(invoke_without_command=True)
+    async def effects(self, ctx: commands.Context):
+        embed = discord.Embed(title="Effect Commands", color=0x2B2D31)
+        embed.add_field(name="effects ae", value="Sends effects for After Effects", inline=False)
+        embed.add_field(name="effects vs", value="Sends effects for videostar", inline=False)
+        await ctx.reply(embed=embed)
+
+    @effects.command()
+    async def ae(self, ctx):
         choices = ["4-Color Gradient", "S_HalfTone", "Gradient Ramp", "S_PseudoColor", "S_FlysEyeHex", "S_WipeTiles", "S_EdgeRays", "S_WipeMoire", "S_WipeDots", "S_WipePixelate", "S_WipePlasma", "S_WipeFlux", "S_GlowDist", "S_Glint", "Glow", "Turbulent Displace", "Wave Warp", "BCC lens blur OBS", "Invert", "Exposure", "BCC Cross Glitch", "BCC LED", "Omino Diffuse", "Omino Squares", "Grid"]
+        raneffect = random.choice(choices)
+        await ctx.reply(raneffect)
+
+    @effects.command()
+    async def vs(self, ctx):
+        choices = ["Select Shift", "Luma Fade", "Contrast", "Swap Hue", "Delete Color", "neXt-Ray", "Solarize", "Rainbow Halo", "E-Sample", "E-Saber", "E-Glitter", "E-Rays", "E-Anime", "E-Aura", "Frozen", "Zoom Blur", "Lens Blur", "Ring Blur", "Light Rays", "Minimax Hex", "Minimax Spin", "LED Image Circle", "LED Image Square", "LED Reveal", "Color Shadow", "Cosmic Wave", "Hole Wipe 1", "Hole Wipe 2", "Rect Wipe 1", "Rect Wipe 2", "*-Bit Wipe", "Glitch A0", "Glitch A1", "Glitch A2", "Glitch A4", "Glitch A5", "Glitch A6", "Glitch A7", "Glitch A8", "Glitch A9", "Blacken", "Hue Shift", "Color SHift", "Video Smear", "Video Melt", "Bevel Edge", "Halftone", "Block Filter", "Outline", "Scanner 1", "Scanner 2", "Edge", "White Edge", "White Lines"]
         raneffect = random.choice(choices)
         await ctx.reply(raneffect)
 
