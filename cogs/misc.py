0e7b3df459e2e3c7b443c54aadbabce271b3d840
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-11-03 10:34:31 +0000

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -265,8 +265,8 @@ class misc(commands.Cog):
 
                 draw.text((417, 292), display, '#000000', font=font2)
                 draw.text((415, 290), display, '#ffffff', font=font2)
-                draw.text((60, 720), about_me_text, '#ffffff', font=font)
-                draw.text((450, 460), f"{prn1} the pronouns {prns}\n{prn3} {title}\n{prn2} edit on {program}", '#ffffff', font=font)
+                draw.text((60, 720), about_me_text, '#ffffff', font=poppins)
+                draw.text((450, 460), f"{prn1} the pronouns {prns}\n{prn3} {title}\n{prn2} edit on {program}", '#ffffff', font=poppins)
                 draw.text((470, 680), "About Me", fill=(255, 255, 255), font=font4)
                 draw.text((640, 410), "Extra Info", fill=(255, 255, 255), font=font4)
 
