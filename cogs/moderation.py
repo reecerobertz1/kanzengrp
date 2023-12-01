400651cc075c026f216925f02a9b6ebf0e371292
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-12-01 09:42:23 +0000

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -22,7 +22,6 @@ class reportmodal(ui.Modal, title='Bug Report'):
         super().__init__(**kwargs)
         self.bot = bot
         self.value = None
-        self.emoji="<:rj:1121909526300479658>"
 
     notified = ui.TextInput(label='Do you want to be notified when its fixed?', placeholder="Yes/No", style=discord.TextStyle.short)
     command = ui.TextInput(label='What command is broken', placeholder="Command name here... example: +about", style=discord.TextStyle.short)
@@ -85,6 +84,7 @@ class Moderation(commands.Cog):
         self.bot = bot
         self.start_time = datetime.datetime.utcnow()
         self.guilds =[1121841073673736215]
+        self.emoji="<:rj:1121909526300479658>"
 
     @commands.command(hidden=True)
     async def suggest(self, ctx, *, suggestion):
