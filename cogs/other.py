2c72217611126c315120926806fba2672d407e53
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-12-01 08:41:54 +0000

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -3,86 +3,11 @@ import random
 import discord
 from discord.ext import commands
 
-class HelloLoop:
-    def __init__(self, bot, channel):
-        self.bot = bot
-        self.channel = channel
-        self.loop = asyncio.get_event_loop()
-        self.hello_task = None
-
-    async def send_hello(self):
-        target_channel = self.bot.get_channel(1125053619893440653)
-        while True:
-            revive = [
-            "<@&1122927309813461143> Hi wake up! come and chat", "<@&1122927309813461143> wakey wakey!", "<@&1122927309813461143> hello start a conversation", "im bored anyone want to chat or play a game? <@&1122927309813461143>",
-            "<@&1122927309813461143>", "<@&1122927309813461143> where is everyone?"
-        ]
-
-            ping = random.choice(revive)
-            await target_channel.send(ping)
-            await asyncio.sleep(86400)
-
-    def start(self):
-        if self.hello_task is None:
-            self.hello_task = self.loop.create_task(self.send_hello())
-
-    def cancel(self):
-        if self.hello_task is not None:
-            self.hello_task.cancel()
-            self.hello_task = None
-
-class pingloop:
-    def __init__(self, bot, channel):
-        self.bot = bot
-        self.channel = channel
-        self.loop = asyncio.get_event_loop()
-        self.hello_task = None
-
-    async def send_ping(self):
-        target_channel = self.bot.get_channel(1122238827973595238)
-        while True:
-            revive = [
-            "<@&1122999575200940083> Hi wake up! come and chat", "<@&1122999575200940083> wakey wakey!", "<@&1122999575200940083> hello start a conversation", "im bored anyone want to chat or play a game? <@&1122999575200940083>",
-            "<@&1122999575200940083>", "<@&1122999575200940083> where is everyone?"
-        ]
-
-            ping = random.choice(revive)
-            await target_channel.send(ping)
-            await asyncio.sleep(86400)
-
 class other(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.hello_loop = None
         self.hidden = True
-
-    @commands.command(hidden=True)
-    async def kanzenrevive(self, ctx):
-        if self.hello_loop is None:
-            self.hello_loop = HelloLoop(self.bot, ctx.channel)
-            self.hello_loop.start()
-            await ctx.send('Started pinging chat revive.')
-
-    @commands.command(hidden=True)
-    async def kanzendie(self, ctx):
-        if self.hello_loop is not None:
-            self.hello_loop.cancel()
-            self.hello_loop = None
-            await ctx.send('Stopped pinging chat revive.')
-
-    @commands.command(hidden=True)
-    async def aurarevive(self, ctx):
-        if self.pingloop is None:
-            self.pingloop = HelloLoop(self.bot, ctx.channel)
-            self.pingloop.start()
-            await ctx.send('Started pinging chat revive.')
-
-    @commands.command(hidden=True)
-    async def auradie(self, ctx):
-        if self.pingloop is not None:
-            self.pingloop.cancel()
-            self.pingloop = None
-            await ctx.send('Stopped pinging chat revive.')
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -102,12 +27,14 @@ class other(commands.Cog):
             await message.channel.send("OMG JIMIN<:chimmy:1128727915664785409>:heartpulse::face_with_peeking_eye::heart_eyes::face_holding_back_tears::pleading_face::sob::weary::tired_face::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::pray_tone1::pinching_hand:<:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> :face_holding_back_tears::face_holding_back_tears::face_holding_back_tears:")
         if message.content.lower() == "kelly":
             await message.channel.send("Jungkook is so jungkook and no one will ever jungkook like jungkook <:boobs:1142888878999613520>")
-        if message.content.lower() == "why is it so dead":
-            await message.channel.send("<@&1134876934585712773> where are you")
-        if message.content.lower() == "where is everyone":
-            await message.channel.send("<@&1134876934585712773> where are you")
-        if message.content.lower() == "anyone there?":
-            await message.channel.send("<@&1134876934585712773> where are you")
+        if "MessageType.premium_guild" in str(message.type):
+                embed = discord.Embed(title=f"Thank you for boosting! {message.author.display_name}!", description="<a:Arrow_1:1145603161701224528> dm staff or lead for your custom role\n<a:Arrow_1:1145603161701224528> go into [#booster-perks](https://discord.com/channels/1121841073673736215/1164318431462563951) to claim the perks\n<a:Arrow_1:1145603161701224528> we really appreciate your support!", color=0x2b2d31)
+                embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar)
+                embed.set_footer(text=f"We are up to {message.guild.premium_subscription_count} boosts!")
+                if message.guild.id == 1121841073673736215:
+                    await message.channel.send(f"{message.author.mention}", embed=embed)
+                else:
+                    pass
 
 async def setup(bot):
     await bot.add_cog(other(bot))
