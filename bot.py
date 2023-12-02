684238af0163cd3efb3c43a5e94d00a604484999
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-12-02 02:39:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ class LalisaBot(commands.Bot):
             intents=intents,
             help_command=None,
             status=discord.Status.online,
-            activity=discord.Game("Hoshi ♡"),
+            activity=discord.Game("Hoshi ?"),
         )
         self._BotBase__cogs = commands.core._CaseInsensitiveDict()
 
