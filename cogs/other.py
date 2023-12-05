bc0e6d9d6563c8424e40db78fffb3baaf26265c0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-12-05 02:28:33 +0000

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -21,7 +21,7 @@ class other(commands.Cog):
         if message.content.lower() == "nani":
             await message.channel.send("i woke up in a new 🔥bugatti🔥")
         if message.content.lower() == "spotify ads":
-            await message.channel.send("follow 0470pm for free spotify premium")
+            await message.channel.send("follow 94zwld for free spotify premium")
         if message.content.lower() == "jess":
             await message.channel.send("https://media0.giphy.com/media/DWPCBPJPBLRsSG0LI2/giphy.gif?cid=ad7b17ba5hahdaf6h1wgccgl9loldyoowq0u3d0c68fisadd&ep=v1_gifs_search&rid=giphy.gif&ct=g")
         if message.content.lower() == "kira":
@@ -30,6 +30,8 @@ class other(commands.Cog):
             await message.channel.send("Jungkook is so jungkook and no one will ever jungkook like jungkook <:boobs:1142888878999613520>")
         if message.content.lower() == "savvi":
             await message.channel.send("SAVVI IS THE FUCKING BEST NO ONE ELSE CAN BEAT HER")
+        if message.content.lower() == "kio":
+            await message.channel.send("kio - savvi's discord e girl kitten sex toy")
         if "MessageType.premium_guild" in str(message.type):
                 embed = discord.Embed(title=f"Thank you for boosting! {message.author.display_name}!", description="<a:Arrow_1:1145603161701224528> dm staff or lead for your custom role\n<a:Arrow_1:1145603161701224528> go into [#booster-perks](https://discord.com/channels/1121841073673736215/1164318431462563951) to claim the perks\n<a:Arrow_1:1145603161701224528> we really appreciate your support!", color=0x2b2d31)
                 embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar)
