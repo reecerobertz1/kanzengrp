d7e568b850ce65edf68a1da685c7d209e1c112d0
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-12-09 10:32:55 +0000

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -301,7 +301,7 @@ class Fun(commands.Cog):
         percentage = random.randint(0, 100)
         await ctx.send(f"{member.mention} is **{percentage}%** gay. 🏳️‍🌈")
 
-    @commands.command(name='giphy',description="Search for gifs", extras="+giphy bts dance")
+    @commands.hybrid_command(name='giphy',description="Search for gifs", extras="+giphy bts dance")
     async def giphy(self, ctx, *, search):
         api_key = "PF48beXJTbUkvh35ThoQ4t1qhyjleLwD"
         url = f"https://api.giphy.com/v1/gifs/search"
@@ -362,7 +362,7 @@ class Fun(commands.Cog):
         avatar_image = avatar_image.resize((160, 160))
         return avatar_image, circle
 
-    @commands.hybrid_command(name='tweet',description="Tweet as yourself with Hoshi", extras="+tweet hello twitter")
+    @commands.command(name='tweet',description="Tweet as yourself with Hoshi", extras="+tweet hello twitter")
     async def tweet(self, ctx, *tweet: str) -> BytesIO:
         tweet_text = " ".join(tweet)
         avatar_url = ctx.author.avatar.url
