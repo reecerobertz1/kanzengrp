152e3e7b401674b1daa887fac0c77e5cbdf9d486
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2023-12-31 01:39:37 +0000

    Update eb.py

--- cogs/eb.py
+++ cogs/eb.py
@@ -995,12 +995,10 @@ class ebmessages(commands.Cog):
         await ctx.send("if you want to become a partner with Editors Block, please go to <#1131005271502753812> and click the `Partnership Info` button and see if you meet our requirements, if you do message <@609515684740988959>")
 
     @app_commands.command(name="kanzengrp", description="apply for kanzengrp")
-    @app_commands.checks.has_permissions(manage_guild=True)
     async def kanzengrp(self, interaction: discord.Interaction, member: discord.Member):
         await interaction.response.send_modal(kapps())
 
     @app_commands.command(name="daegutown", description="apply for daegutown")
-    @app_commands.checks.has_permissions(manage_guild=True)
     async def daegutown(self, interaction: discord.Interaction, member: discord.Member):
         await interaction.response.send_modal(dapps())
 
