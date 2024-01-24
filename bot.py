28c50cbce90b7d02d139e91609868ac5dd8425f7
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-01-24 14:32:46 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,8 +63,6 @@ class LalisaBot(commands.Bot):
             await conn.execute('''CREATE TABLE IF NOT EXISTS setup (guild_id BIGINT PRIMARY KEY, activated BOOL, top_20_role_id BIGINT, bronze_role_id BIGINT, silver_role_id BIGINT, gold_role_id BIGINT, diamond_role_id BIGINT, plat_role_id BIGINT, elite_role_id BIGINT)''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS bank (wallet INTEGER, bank INTEGER, maxbank INTEGER, user INTEGER, job TEXT)''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS inventory (user INTEGER,item TEXT,quantity INTEGER DEFAULT 0,PRIMARY KEY (user, item))''')
-            await conn.execute('''CREATE TABLE IF NOT EXISTS profiles (user_id INTEGER PRIMARY KEY, about_me TEXT, instagram_username TEXT, pronouns TEXT, banner_url TEXT, background_url TEXT)''')
-            await conn.execute('''CREATE TABLE IF NOT EXISTS user_profiles (user_id INTEGER PRIMARY KEY, discord_id INTEGER, bias_list TEXT)''') 
             await conn.execute('''CREATE TABLE IF NOT EXISTS warning (member_id INTEGER, guild_id INTEGER ,reasons TEXT, warnings INTEGER)''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS afk (user_id bigint PRIMARY KEY , reason text , time timestamp with time zone)''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS staffrep(member_id INTEGER, helped INTEGER, count INTEGER, guild_id INTEGER)''')
