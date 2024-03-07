19ccd9de77063ffdccf444620ec1894c602fa69d
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-03-07 08:44:19 +0000

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -165,10 +165,6 @@ class other(commands.Cog):
             await message.channel.send("i woke up in a new 🔥bugatti🔥")
         if message.content.lower() == "spotify ads":
             await message.channel.send("follow 94zwld for free spotify premium")
-        if message.content.lower() == "jess":
-            await message.channel.send("https://media0.giphy.com/media/DWPCBPJPBLRsSG0LI2/giphy.gif?cid=ad7b17ba5hahdaf6h1wgccgl9loldyoowq0u3d0c68fisadd&ep=v1_gifs_search&rid=giphy.gif&ct=g")
-        if message.content.lower() == "kira":
-            await message.channel.send("OMG JIMIN<:chimmy:1128727915664785409>:heartpulse::face_with_peeking_eye::heart_eyes::face_holding_back_tears::pleading_face::sob::weary::tired_face::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::heartpulse::pray_tone1::pinching_hand:<:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> <:chimmy:1128727915664785409> :face_holding_back_tears::face_holding_back_tears::face_holding_back_tears:")
         if message.content.lower() == "kelly":
             await message.channel.send("Jungkook is so jungkook and no one will ever jungkook like jungkook <:boobs:1142888878999613520>")
         if message.content.lower() == "kio":
