2ef0bd8662945a30064f69c7dcb20c541ce61cb1
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-04-21 03:34:02 +0100

    Update levels.py

--- cogs/levels.py
+++ cogs/levels.py
@@ -950,14 +950,9 @@ class levels(commands.Cog):
         else:
             async with self.bot.pool.acquire() as conn:
                 async with conn.cursor() as cursor:
-                    await cursor.execute(query, (levels['mora'] + amount, member_id))
+                    await cursor.execute(query, (levels['mora'] + amount, member_id, ))
                     await conn.commit()
                 await self.bot.pool.release(conn)
-
-    @app_commands.command(name="addmora", description="Add mora to a zennie")
-    async def addmora(self, interaction: discord.Interaction, member: discord.Member, amount: int):
-        await self.add_mora(member.id, amount)
-        await interaction.response.send_message(f"Added <:mora:1230914532675813508> **{amount}** Mora to {member.mention}")
 
     async def add_stardust(self, member_id: int, amount: int) -> None:
         query = '''UPDATE levels SET stardust = ? WHERE member_id = ?'''
@@ -973,15 +968,19 @@ class levels(commands.Cog):
         else:
             async with self.bot.pool.acquire() as conn:
                 async with conn.cursor() as cursor:
-                    await cursor.execute(query, (levels['stardust'] + amount, member_id))
+                    await cursor.execute(query, (levels['stardust'] + amount, member_id, ))
                     await conn.commit()
                 await self.bot.pool.release(conn)
+
+    @app_commands.command(name="addmora", description="Add mora to a zennie")
+    async def addmora(self, interaction: discord.Interaction, member: discord.Member, amount: int):
+        await self.add_mora(member.id, amount)
+        await interaction.response.send_message(f"Added <:mora:1230914532675813508> **{amount}** Mora to {member.mention}")
 
     @app_commands.command(name="addstardust", description="Add stardust to a zennie")
     async def addstardust(self, interaction: discord.Interaction, member: discord.Member, amount: int):
         await self.add_stardust(member.id, amount)
         await interaction.response.send_message(f"Added <:stardust:1230256970859155486> **{amount}** Stardust to {member.mention}")
-
 
 async def setup(bot):
     await bot.add_cog(levels(bot))
