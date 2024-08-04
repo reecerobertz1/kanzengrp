9f5193241e8619dacb245046e1671989ef976291
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-08-04 21:01:56 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -7,28 +7,27 @@ class welcandleave(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.start_time = datetime.datetime.utcnow()
-        self.kanzen_id = 1131003330810871979
-        self.kanzen_welcome = 1258713117366681711
+        self.kanzen_id = 1134736053803159592
+        self.kanzen_welcome = 1134746303390302238
         self.hidden = True
         self.emoji = "<:shooky:1121909564799987722>"
 
     @commands.Cog.listener()
     async def on_member_join(self ,member: discord.Member):
         if member.guild.id == self.kanzen_id:
-            embed = discord.Embed(title="welcome to kanzengrp!", color=0xb4d4b4, description=f"> • make sure to read our [server rules](https://discord.com/channels/1131003330810871979/1131005271502753812)\n> • apply for kanzen [here](https://discord.com/channels/1131003330810871979/1131005271502753812)\n> • get roles in <id:customize>")
-            role = member.guild.get_role(1131016147282710679)  
+            embed = discord.Embed(title="Welcome to lyra!", color=0xFEF7E5, description=f"• Read the rules in <#1134744553425993799>\n• Get some roles in <#1134771998896181289>")
+            role = member.guild.get_role(1134797836135964723)  
             channel = self.bot.get_channel(self.kanzen_welcome)
             embed.set_thumbnail(url=member.display_avatar.url)
-            embed.set_image(url="https://cdn.discordapp.com/attachments/1254095017878753281/1258717183203344435/welcome_image_00000.png?ex=66890f3a&is=6687bdba&hm=aa21f03c690969b974af3ed268705cb0a81ba4a5d94af30fc36495d9a34a6eb7&")
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1134736054637838490/1137359950750961724/form_gfx_w_grains.png?ex=66b0c3ff&is=66af727f&hm=a8f1172d9b51a678101b5632b16bab4160228bd1fcfd9120c0d9fdc64ea78844&")
             await channel.send(f'{member.mention}', embed=embed)
             await member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         if member.guild.id == self.kanzen_id:
-            embed = discord.Embed(title=f"{member.display_name} has left Kanzen!", color=0x5e9d5e, description="We will miss you !")
+            embed = discord.Embed(title=f"Thank you for joining lyra", color=0x5e9d5e, description="We hope to see you again soon")
             embed.set_thumbnail(url=member.display_avatar.url)
-            embed.set_footer(text='Hope to see you again <3')
             channel = self.bot.get_channel(self.kanzen_welcome)
             await channel.send(f'{member.mention}', embed=embed)
 
