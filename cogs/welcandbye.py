c2aee2afc105dfbbc83ced1478b6d114831cc309
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-08-13 20:34:48 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -14,6 +14,10 @@ class welcandleave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self ,member: discord.Member):
+        public = 835498963418480650
+        role = 694016195090710579
+        stored_guild_id = 694010548605550675
+        scout_guild_id = 835495688832811039
         if member.guild.id == self.kanzen_id:
             embed = discord.Embed(title="Welcome to lyra!", color=0xFEF7E5, description=f"• Read our [rules](https://discord.com/channels/1134736053803159592/1134744553425993799) and get some [roles](https://discord.com/channels/1134736053803159592/1134771998896181289)\n• Chat with other members [here](https://discord.com/channels/1134736053803159592/1134742133421658193)\n• Get to know our [leads](https://discord.com/channels/1134736053803159592/1207118201897492480) & [staff](https://discord.com/channels/1134736053803159592/1134802970031161355)")
             role = member.guild.get_role(1134797836135964723)  
@@ -22,11 +26,7 @@ class welcandleave(commands.Cog):
             embed.set_image(url="https://cdn.discordapp.com/attachments/1134736054637838490/1137359950750961724/form_gfx_w_grains.png?ex=66b0c3ff&is=66af727f&hm=a8f1172d9b51a678101b5632b16bab4160228bd1fcfd9120c0d9fdc64ea78844&")
             await channel.send(f'{member.mention}', embed=embed)
             await member.add_roles(role)
-        public = 835498963418480650
-        role = 694016195090710579
-        stored_guild_id = 694010548605550675
-        scout_guild_id = 835495688832811039
-        if member.guild.id == stored_guild_id:
+        elif member.guild.id == stored_guild_id:
             await member.add_roles(member.guild.get_role(role))
             embed = discord.Embed(title='<a:shookylove:1082786001958744224> __Welcome to chroma!__', color=0x2b2d31, description=f"{member.name} has joined!\n> • Please make sure to read [our rules](https://discord.com/channels/694010548605550675/725373131220320347)\n> • Introduce yourself [here](https://discord.com/channels/694010548605550675/727875317439528982)\n> • Questions? ping <@&753678720119603341> or <@&739513680860938290>\nEnjoy your time here in chroma!")
             channel = self.bot.get_channel(725389930607673384)
@@ -50,13 +50,13 @@ class welcandleave(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
+        stored_guild_id = 694010548605550675
         if member.guild.id == self.kanzen_id:
             embed = discord.Embed(title=f"Thank you for joining lyra", color=0xFEF7E5, description="We hope to see you again soon")
             embed.set_thumbnail(url=member.display_avatar.url)
             channel = self.bot.get_channel(self.kanzen_welcome)
             await channel.send(f'{member.mention}', embed=embed)
-        stored_guild_id = 694010548605550675
-        if member.guild.id == stored_guild_id:
+        elif member.guild.id == stored_guild_id:
             await self.delete_rank(userid=member.id)
             embed = discord.Embed(title="Member left!", color=0x2b2d31, description=f"{member.mention} has left the discord.")
             embed.set_thumbnail(url=member.display_avatar.url)
