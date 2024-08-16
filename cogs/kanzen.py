d28e0a7fccba747f49f9f2a213d4efa237441502
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-08-16 20:08:51 +0100

    Update kanzen.py

--- cogs/kanzen.py
+++ cogs/kanzen.py
@@ -31,11 +31,10 @@ class infoview(discord.ui.View):
 class reportmember(ui.Modal, title='Report'):
     reported = ui.TextInput(label='Who are you reporting?', placeholder="Enter instagram + discord user", style=discord.TextStyle.short)
     why  = ui.TextInput(label='Why are you reporting them?', placeholder="Tell us here", style=discord.TextStyle.long)
-    dm = ui.TextInput(label="Can we dm you for more info?", placeholder="yes or no", style=discord.TextStyle.short, required=False)
     user = ui.TextInput(label="Instagram / discord username", placeholder="Leave blank to stay anonymous", style=discord.TextStyle.short, required=False)
     async def on_submit(self, interaction: discord.Interaction):
         if self.user.value:
-            description=f"reported member:\n{self.reported.value}\n\nwhy was they reported:\n{self.why.value}\n\nwho reported:\n{interaction.user.mention}\n\nyou can dm them for more information\n-# please react to this report message once the issue has been resolved!"
+            description=f"reported member:\n{self.reported.value}\n\nwhy was they reported:\n{self.why.value}\n\nwho reported:\n{interaction.user.mention}\n-# please react to this report message once the issue has been resolved!"
         else:
             description=f"reported member:\n{self.reported.value}\n\nwhy was they reported?\n{self.why.value}"
         embed = discord.Embed(description=description, color=0x2b2d31)
