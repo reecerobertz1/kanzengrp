a62d3fedafc808a58e7db02940d579ea706d63dd
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-09-30 13:40:37 +0100

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -83,10 +83,27 @@ class other(commands.Cog):
             )
             chromaembed.set_thumbnail(url=message.author.display_avatar.url)
 
+            chroma2embed = discord.Embed(
+                title="Thank you for boosting",
+                description=(
+                    f"Thank you {message.author.name} for boosting!\n"
+                    "・Your support is greatly appreciated\n"
+                    "・Head to <#853241710658584586> to claim our perks"
+                ),
+                color=0x2b2d31
+            )
+            chroma2embed.set_footer(
+                text=f"We now have {message.guild.premium_subscription_count} boosts!",
+                icon_url=message.guild.icon.url if message.guild.icon else None
+            )
+            chroma2embed.set_thumbnail(url=message.author.display_avatar.url)
+
             if message.guild.id == 1134736053803159592:
                 await message.channel.send(f"{message.author.mention}", embed=embed)
             elif message.guild.id == 835495688832811039:
                 await message.channel.send(f"{message.author.mention}", embed=chromaembed)
+            elif message.guild.id == 694010548605550675:
+                await message.channel.send(f"{message.author.mention}", embed=chroma2embed)
             else:
                 pass
         afk = await self.check_afk(message.author.id)
