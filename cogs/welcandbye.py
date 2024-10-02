f210de372aa43431e08db622ec368bb712ae73b4
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-10-02 12:59:49 +0100

    Update welcandbye.py

--- cogs/welcandbye.py
+++ cogs/welcandbye.py
@@ -18,6 +18,9 @@ class welcandleave(commands.Cog):
         role = 694016195090710579
         stored_guild_id = 694010548605550675
         scout_guild_id = 835495688832811039
+
+        # LYRA WELCOME EMBED
+
         if member.guild.id == self.kanzen_id:
             embed = discord.Embed(title="Welcome to lyra!", color=0xFEF7E5, description=f"• Read our [rules](https://discord.com/channels/1134736053803159592/1134744553425993799) and get some [roles](https://discord.com/channels/1134736053803159592/1134771998896181289)\n• Chat with other members [here](https://discord.com/channels/1134736053803159592/1134742133421658193)\n• Get to know our [leads](https://discord.com/channels/1134736053803159592/1207118201897492480) & [staff](https://discord.com/channels/1134736053803159592/1134802970031161355)")
             role = member.guild.get_role(1134797836135964723)  
@@ -26,6 +29,9 @@ class welcandleave(commands.Cog):
             embed.set_image(url="https://cdn.discordapp.com/attachments/1134736054637838490/1137359950750961724/form_gfx_w_grains.png?ex=66b0c3ff&is=66af727f&hm=a8f1172d9b51a678101b5632b16bab4160228bd1fcfd9120c0d9fdc64ea78844&")
             await channel.send(f'{member.mention}', embed=embed)
             await member.add_roles(role)
+
+        # CHROMA MEMBERS WELCOME EMBED
+
         elif member.guild.id == stored_guild_id:
             await member.add_roles(member.guild.get_role(role))
             embed = discord.Embed(title='<a:shookylove:1082786001958744224> __Welcome to chroma!__', color=0x2b2d31, description=f"{member.name} has joined!\n> • Please make sure to read [our rules](https://discord.com/channels/694010548605550675/725373131220320347)\n> • Introduce yourself [here](https://discord.com/channels/694010548605550675/727875317439528982)\n> • Questions? ping <@&753678720119603341> or <@&739513680860938290>\nEnjoy your time here in chroma!")
@@ -33,13 +39,18 @@ class welcandleave(commands.Cog):
             embed.set_thumbnail(url=member.display_avatar.url)
             await channel.send(f'{member.mention}', embed=embed)
             channel2 = self.bot.get_channel(694010549532360726)
-            await channel2.send(f"{member.mention} welcome to chroma!\n• get roles in <id:customize>\n• Our logos are in <#725373131220320347>\n• Introduce yourself in <#727875317439528982>\nAny questions, ping staff or leads <a:3b56330f710c3a978f27c9cc7e099180:836940737123057705>")
+            await channel2.send(f"{member.mention} welcome to chroma!\n• get roles in <id:customize>\n・ Our logos are in <#725373131220320347>\n・ Introduce yourself in <#727875317439528982>\nAny questions, ping staff or leads <a:3b56330f710c3a978f27c9cc7e099180:836940737123057705>")
+            await self.add_member(member.id)
+
+        # CHROMA COMMUNITY WELCOME EMBED
+
         elif member.guild.id == scout_guild_id:
             guild = self.bot.get_guild(694010548605550675)
             guild2 = self.bot.get_guild(835495688832811039)
-            embed2 = discord.Embed(title="welcome!", color=0x303136, description=f"{member.mention} has joined the server!\n• read our <#1283439723053977730>!\n• get editing help in <#1020005171645915196>\n• talk to other editors <#836647673595428925>")
+            embed2 = discord.Embed(title="welcome!", color=0x303136, description=f"• Read our [information](https://discord.com/channels/835495688832811039/1283439723053977730) and [apply](https://discord.com/channels/835495688832811039/1252726042964136026)\n• Chat with other editors [here](https://discord.com/channels/835495688832811039/836647673595428925)\n• Get editing help in [this channel](https://discord.com/channels/835495688832811039/1020005171645915196)")
             embed2.set_footer(text='thanks for wanting to join chroma! <3', icon_url=member.display_avatar.url)
             embed2.set_thumbnail(url=member.display_avatar.url)
+            embed2.set_image(url="https://cdn.discordapp.com/attachments/1055168099252437094/1291004710182064211/chroma_partner_banner_00000_1.png?ex=66fe8554&is=66fd33d4&hm=5516aea99c974e800ba5558f3dcf732beaec34464adfaaaa9b411d155f2432da&")
             channel2 = self.bot.get_channel(836251337649160256)
             await channel2.send(f'{member.mention}', embed=embed2)
             member2 = guild.get_member(member.id)
@@ -48,20 +59,42 @@ class welcandleave(commands.Cog):
             else:
                 await member.add_roles(member.guild.get_role(836244165637046283))
 
+    async def add_member(self, member_id: int, xp=5) -> None:
+        query = '''INSERT INTO chromalevels (member_id, xp, messages, color) VALUES (?, ?, ?, ?)'''
+        async with self.bot.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(query, (member_id, xp, 1, '#c45a72'))
+                await conn.commit()
+            await self.bot.pool.release(conn)
+
+    async def remove_member(self, member_id: int) -> None:
+        async with self.bot.pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute('DELETE FROM chromalevels WHERE member_id = $1', member_id)
+                await conn.commit()
+            await self.bot.pool.release(conn)
+
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         stored_guild_id = 694010548605550675
+
+        # LYRA LEAVE EMBED
+
         if member.guild.id == self.kanzen_id:
             embed = discord.Embed(title=f"Thank you for joining lyra", color=0xFEF7E5, description="We hope to see you again soon")
             embed.set_thumbnail(url=member.display_avatar.url)
             channel = self.bot.get_channel(self.kanzen_welcome)
             await channel.send(f'{member.mention}', embed=embed)
+
+        # CHROMA LEAVE EMBED
+
         elif member.guild.id == stored_guild_id:
             embed = discord.Embed(title="Member left!", color=0x2b2d31, description=f"{member.mention} has left the discord.")
             embed.set_thumbnail(url=member.display_avatar.url)
             embed.set_footer(text='i hope to see you again!', icon_url='https://cdn.discordapp.com/attachments/799984745772875786/800015677653909504/yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.png')
             channel = self.bot.get_channel(725389930607673384)
             await channel.send(embed=embed)
+            await self.remove_member(member.id)
 
 async def setup(bot):
     await bot.add_cog(welcandleave(bot))
