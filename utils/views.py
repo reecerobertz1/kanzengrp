d8c40817973555402af4e01a172d3da180e5267d
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2024-11-14 04:33:58 +0000

    Update views.py

--- utils/views.py
+++ utils/views.py
@@ -1,39 +1,61 @@
-from discord import ui, Embed
+from discord import ui, Embed, ButtonStyle
 from discord.interactions import Interaction
-from collections import deque
 from typing import List
 
 class Paginator(ui.View):
     def __init__(self, embeds: List[Embed]):
         self._embeds = embeds
-        self._queue = deque(embeds)
         self._len = len(embeds)
-        self._index = 1
-        self._initial = embeds[0].set_footer(text=f"Page {self._index} / {self._len}")
-
+        self._index = 0
+        self._initial = embeds[self._index].set_footer(text=f"Page {self._index + 1}/{self._len}")
         super().__init__(timeout=90)
+        self.update_page_number_label()
+
+    def update_page_number_label(self):
+        """Helper function to update the page number label"""
+        self.page_number.label = f"{self._index + 1}/{self._len}"
+
+    @ui.button(label='<<')
+    async def first_embed(self, interaction: Interaction, button: ui.Button):
+        self._index = 0
+        embed = self._embeds[self._index]
+        self.update_page_number_label()
+        embed.set_footer(text=f"Page {self._index + 1}/{self._len}")
+        await interaction.response.edit_message(embed=embed, view=self)
 
     @ui.button(label='<')
-    async def previous_embed(self, interaction: Interaction, _):
-        self._queue.rotate(1)
-        if self._index == 1:
-            self._index = self._len
-        else:
+    async def previous_embed(self, interaction: Interaction, button: ui.Button):
+        if self._index > 0:
             self._index -= 1
-        embed = self._queue[0]
-        embed.set_footer(text=f"Page {self._index} / {self._len}")
-        await interaction.response.edit_message(embed=embed)
+        else:
+            self._index = self._len - 1
+        embed = self._embeds[self._index]
+        self.update_page_number_label()
+        embed.set_footer(text=f"Page {self._index + 1}/{self._len}")
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @ui.button(label="1/1", disabled=True, style=ButtonStyle.secondary)
+    async def page_number(self, interaction: Interaction, button: ui.Button):
+        await interaction.response.send_message("This button just shows the page number.", ephemeral=True)
 
     @ui.button(label='>')
-    async def next_embed(self, interaction: Interaction, _):
-        self._queue.rotate(-1)
-        if self._index == self._len:
-            self._index = 1
-        else:
+    async def next_embed(self, interaction: Interaction, button: ui.Button):
+        if self._index < self._len - 1:
             self._index += 1
-        embed = self._queue[0]
-        embed.set_footer(text=f"Page {self._index} / {self._len}")
-        await interaction.response.edit_message(embed=embed)
+        else:
+            self._index = 0
+        embed = self._embeds[self._index]
+        self.update_page_number_label()
+        embed.set_footer(text=f"Page {self._index + 1}/{self._len}")
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @ui.button(label='>>')
+    async def last_embed(self, interaction: Interaction, button: ui.Button):
+        self._index = self._len - 1
+        embed = self._embeds[self._index]
+        self.update_page_number_label()
+        embed.set_footer(text=f"Page {self._index + 1}/{self._len}")
+        await interaction.response.edit_message(embed=embed, view=self)
 
     @property
     def initial(self) -> Embed:
