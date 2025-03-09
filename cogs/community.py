1fbc834865eb578688d1d540ca0277a4794daac1
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2025-03-09 23:15:19 +0000

    Update community.py

--- cogs/community.py
+++ cogs/community.py
@@ -10,7 +10,7 @@ class verify(discord.ui.View):
         super().__init__(timeout=None)
         self.value = None
 
-    @discord.ui.button(label="Verify", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label="Verify", style=discord.ButtonStyle.red)
     async def verify(self, interaction: discord.Interaction, button: discord.Button):
         letters = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
         numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
