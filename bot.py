7f8f3d71e1d2faf3f38392d2d4e7a2d942f4f4d2
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2025-03-27 16:34:27 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ class LalisaBot(commands.Bot):
         self.pool = await asqlite.create_pool('databases/levels.db')
 
         async with self.pool.acquire() as conn:
-            await conn.execute('''CREATE TABLE IF NOT EXISTS levelling (guild_id BIGINT, member_id BIGINT, xp INTEGER, messages INTEGER, color TEXT, image TEXT, decor TEXT, format INTEGER, PRIMARY KEY(guild_id, member_id))''')
+            await conn.execute('''CREATE TABLE IF NOT EXISTS levelling (guild_id BIGINT, member_id BIGINT, xp INTEGER, messages INTEGER, color TEXT, color2 TEXT, image TEXT, decor TEXT, format INTEGER, PRIMARY KEY(guild_id, member_id))''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS levels (guild_id BIGINT, member_id INTEGER, xp INTEGER, messages INTEGER, bar_color TEXT, PRIMARY KEY (guild_id, member_id))''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS decors (member_id BIGINT, unlocked INTEGER, selected INTEGER, currency INTEGER, PRIMARY KEY(member_id))''')
             await conn.execute('''CREATE TABLE IF NOT EXISTS settings (guild_id BIGINT, chatxp INTEGER, voicexp INTEGER, dailyxp TEXT, top20 INTEGER, reprole INTEGER, channels TEXT, voicechannels TEXT, levels INTEGER, PRIMARY KEY(guild_id))''')
