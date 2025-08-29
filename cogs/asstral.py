b202189536d3c39e1d7c705accf91f2c31a91f35
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2025-08-29 18:00:02 +0100

    Update asstral.py

--- cogs/asstral.py
+++ cogs/asstral.py
@@ -616,7 +616,7 @@ class Asstral(commands.Cog):
             member_id = member.id
             levels = await self.get_member_levels(member_id)
             xp_amount = randint(100, 300)
-            await self.add_xp(member_id, guild_id, xp_amount, levels)
+            await self.add_xp(member_id, xp_amount, levels)
             await ctx.reply(f"You claimed your **daily reward** and earned `{xp_amount} XP`!")
 
     @dailies.error
