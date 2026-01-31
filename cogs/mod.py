c472f544c7dca1b518cd4ea40da9885f1cbc9e79
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2026-01-31 19:23:16 +0000

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -7,7 +7,7 @@ class mod(commands.Cog):
         self.bot = bot
         self.color = 0x2b2d31
 
-    @commands.hybrid_command(name="dm", aliases=["message"], description="Dm a user through hoshi", extras="+dm @member (message) : alias +message")
+    @commands.command(name="dm")
     async def dm(self, ctx, member: discord.Member, *, message: str):
         await member.send(message)
         await ctx.send(f"i have successfully messaged {member.mention}\n{message}")   
