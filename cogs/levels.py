e831f838e28abdf2a317ab032efdae51898b744f
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2026-02-25 16:23:46 +0000

    Update levels.py

--- cogs/levels.py
+++ cogs/levels.py
@@ -616,7 +616,7 @@ class levels(commands.Cog):
         dark = ImageEnhance.Brightness(bg)
         bg_dark = dark.enhance(1.5)
         bg_blurred = bg_dark.filter(ImageFilter.GaussianBlur(radius=20))
-        number = randint(1, 3)
+        number = randint(1, 4)
         mask = Image.open(f"./assets/rankcard/{number}.png").resize((1500, 500)).convert("L")
         inverted_mask = ImageOps.invert(mask)
         bg_frosted = Image.composite(bg_blurred, Image.new("RGBA", bg.size, "white"), inverted_mask)
