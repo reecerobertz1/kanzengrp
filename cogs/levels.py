7a655089d0a67fa9db73c743dee343d759183096
reecerobertz1 <125633685+reecerobertz1@users.noreply.github.com>
2026-03-28 20:57:13 +0000

    Update levels.py

--- cogs/levels.py
+++ cogs/levels.py
@@ -761,7 +761,6 @@ class levels(commands.Cog):
             card.seek(0)
             ext = 'gif' if header[:3] == b'GIF' else 'png'
             view = RankCardConfig(member.id, bot=self.bot)
-            view.add_item(DarkMode(member.id, bot=self.bot))
             await interaction.followup.send(
                 file=discord.File(card, f'card.{ext}'),
                 view=view)
